23f497f36c0f126bafc43737a800003d85f26a2a
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-04 15:39:42 +0900

    feat: add about page route in router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );

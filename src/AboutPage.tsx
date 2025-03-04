44eaa131fbb156ec39b1c15c8640fa1044172ec2
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-04 15:38:41 +0900

    feat: add AboutPage

---
+++ src/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router';
+
+export default function AboutPage() {
+  return (
+    <div>
+      AboutPage
+      <div>
+        <NavLink to="/">to HomePage</NavLink>
+      </div>
+    </div>
+  );
+}

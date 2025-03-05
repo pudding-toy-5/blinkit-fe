5c5219a1264c053b5f22c38b43b4faf95821fda8
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-05 17:52:25 +0900

    style: add style to SubPageHeader and change button text

--- src/shared/ui/SubPageHeader/index.tsx
+++ src/shared/ui/SubPageHeader/index.tsx
@@ -5,9 +5,11 @@ export interface SubPageHeaderProps {
 
 const SubPageHeader: React.FC<SubPageHeaderProps> = ({ backLink, title }) => {
   return (
-    <header>
-      <button aria-label="뒤로가기">뒤로 가기</button>
-      <h1>{title}</h1>
+    <header className="flex flex-row h-12 px-4 py-3">
+      <button className="w-6 h-6" aria-label="뒤로가기">
+        {'<'}
+      </button>
+      <h1 className="ml-2 text-base">{title}</h1>
     </header>
   );
 };

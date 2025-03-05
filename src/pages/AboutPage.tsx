ca75649f20e82f8448897594a67a70eb4c40d73f
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-05 17:59:34 +0900

    feat: add SubPageHeader to AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,8 +1,11 @@
 import { NavLink } from 'react-router';
 
+import SubPageHeader from '@/shared/ui/SubPageHeader';
+
 const AboutPage: React.FC = () => {
   return (
     <div>
+      <SubPageHeader backLink="/" title="About Page" />
       AboutPage
       <div>
         <NavLink to="/">to HomePage</NavLink>

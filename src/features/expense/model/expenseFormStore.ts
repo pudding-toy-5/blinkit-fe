d30620ad7b797d94419e05ff6869fce2893a2c84
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-06 13:00:40 +0900

    lint: lint expenseFormStore

--- src/features/expense/model/expenseFormStore.ts
+++ src/features/expense/model/expenseFormStore.ts
@@ -8,23 +8,23 @@ const initialExpenseFormState: ExpenseFormState = {
   amount: 0,
 };
 
-export const useExpenseFormStore = create<ExpenseFormState & ExpenseFormActions>(
-  (set) => ({
-    ...initialExpenseFormState,
-    setDate: (value) => {
-      set({ date: value });
-    },
-    setCategory: (value) => {
-      set({ category: value });
-    },
-    setMemo: (value) => {
-      set({ memo: value });
-    },
-    setAmount: (value) => {
-      set({ amount: value });
-    },
-    resetForm: () => {
-      set({ ...initialExpenseFormState });
-    },
-  })
-);
+export const useExpenseFormStore = create<
+  ExpenseFormState & ExpenseFormActions
+>((set) => ({
+  ...initialExpenseFormState,
+  setDate: (value) => {
+    set({ date: value });
+  },
+  setCategory: (value) => {
+    set({ category: value });
+  },
+  setMemo: (value) => {
+    set({ memo: value });
+  },
+  setAmount: (value) => {
+    set({ amount: value });
+  },
+  resetForm: () => {
+    set({ ...initialExpenseFormState });
+  },
+}));

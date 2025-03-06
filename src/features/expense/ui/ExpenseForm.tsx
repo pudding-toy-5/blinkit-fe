51677efdb98ecf5bb9e1cd19c0e0cfd87d9ba365
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-06 15:41:48 +0900

    style: add styling in ExpenseForm

--- src/features/expense/ui/ExpenseForm.tsx
+++ src/features/expense/ui/ExpenseForm.tsx
@@ -2,31 +2,36 @@ import RightArrowIcon from '@/shared/ui/RightArrowIcon';
 
 const ExpenseForm: React.FC = () => {
   return (
-    <form>
-      <div>
+    <form className='flex flex-col mx-4 mt-4 h-screen'>
+      <div className='flex flex-row '>
         <label>날짜</label>
-        <button>
+        <button className='flex flex-row ml-auto'>
           <p>2025년 2월 28일</p>
           <RightArrowIcon />
         </button>
       </div>
-      <div>
+      <div className='flex flex-row mt-5'>
         <label>카테고리</label>
-        <button>
+        <button className='flex flex-row ml-auto'>
           <p>식비</p>
           <RightArrowIcon />
         </button>
       </div>
-      <div>
+      <div className='flex flex-col mt-5'>
         <label>메모</label>
-        <input type='text' />
+        <input type='text' className='my-2 h-38' />
         <p>0/120</p>
       </div>
-      <div>
+      <div className='flex flex-col mt-5'>
         <label>금액</label>
         <input type='text' />
       </div>
-      <button type='submit'>저장</button>
+      <button
+        type='submit'
+        className='mt-auto mb-5 p-3 bg-black text-white rounded-lg'
+      >
+        저장
+      </button>
     </form>
   );
 };

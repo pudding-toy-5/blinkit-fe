eb6d68a3cdde6477186124387d0ec8e7e180075e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-06 15:45:10 +0900

    feat: add ExpenseForm to AboutPage
    remove NavLink and title

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,15 +1,11 @@
-import { NavLink } from 'react-router';
-
 import SubPageHeader from '@/shared/ui/SubPageHeader';
+import ExpenseForm from '@/features/expense/ui/ExpenseForm';
 
 const AboutPage: React.FC = () => {
   return (
     <div className='max-w-sm mx-auto h-screen bg-gray-100 flex flex-col'>
       <SubPageHeader backLink='/' title='About Page' />
-      AboutPage
-      <div>
-        <NavLink to='/'>to HomePage</NavLink>
-      </div>
+      <ExpenseForm />
     </div>
   );
 };

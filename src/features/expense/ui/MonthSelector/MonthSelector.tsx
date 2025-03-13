e741f7cca8bad24d589ce94b716970fc678212d8
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-13 09:13:30 +0900

    feat: add basic drawer to MonthSelector button

--- src/features/expense/ui/MonthSelector/MonthSelector.tsx
+++ src/features/expense/ui/MonthSelector/MonthSelector.tsx
@@ -1,5 +1,12 @@
 import { Button } from '@/components/ui/button';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 import { useExpenseStore } from '@/features/expense/model/expenseStore';
 
@@ -11,7 +18,17 @@ const MonthSelector = () => {
       <Button variant='ghost' size='icon' aria-label='이전 월로 이동'>
         <ChevronLeft />
       </Button>
-      <Button variant='link'>{month}월</Button>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button variant='link'>{month}월</Button>
+        </DrawerTrigger>
+        <DrawerContent className='flex items-center'>
+          <DrawerHeader>
+            <DrawerTitle>조회 월 선택</DrawerTitle>
+          </DrawerHeader>
+          <div className='p-4'>조회 월 선택 몸통</div>
+        </DrawerContent>
+      </Drawer>
       <Button variant='ghost' size='icon' aria-label='다음 월로 이동'>
         <ChevronRight />
       </Button>

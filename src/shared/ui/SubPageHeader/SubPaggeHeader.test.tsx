71e5e44830c0358f6059eba2809d8d3ddadd1530
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-13 16:03:29 +0900

    move: move SubPageHeader and test to directory

--- src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
+++ src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
@@ -1,7 +1,9 @@
 import { describe, it, expect } from 'vitest';
 import { render } from '@testing-library/react';
 
-import SubPageHeader, { SubPageHeaderProps } from '../SubPageHeader';
+import SubPageHeader, {
+  SubPageHeaderProps,
+} from '../SubPageHeader/SubPageHeader';
 
 describe('SubPageHeader', () => {
   const renderSubPageHeader = ({ backLink, title }: SubPageHeaderProps) =>

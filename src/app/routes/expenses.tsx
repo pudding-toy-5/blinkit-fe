1b9c6bd69d9ce6be0f71b2aa316e4c23342c6a3f
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-17 14:53:42 +0900

    refact: add DailyExpenseList to routes/expenses

--- src/app/routes/expenses.tsx
+++ src/app/routes/expenses.tsx
@@ -1,16 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router';
 
 import Layout from '@/shared/ui/layout/Layout';
-import MonthSelector from '@/features/expense/ui/MonthSelector';
+import Logo from '@/shared/ui/icons/Logo';
 import { Button } from '@/components/ui/button';
 import { Settings } from 'lucide-react';
-import Logo from '@/shared/ui/icons/Logo';
+import MonthSelector from '@/features/expense/ui/MonthSelector';
+import DailyExpenseList from '@/features/expense/ui/DailyExpenseList';
+
+import { useExpenseStore } from '@/features/expense/model/expenseStore';
+import { DailyExpense } from '@/features/expense/model/types';
 
 export const Route = createFileRoute('/expenses')({
   component: ExpensesPage,
 });
 
 export function ExpensesPage() {
+  const dailyExpenses: DailyExpense[] = useExpenseStore(
+    (state) => state.dailyExpenses
+  );
+
   return (
     <Layout>
       <header className='flex flex-row items-center px-5 py-4'>
@@ -19,11 +27,12 @@ export function ExpensesPage() {
           <Settings className='!size-6' />
         </Button>
       </header>
-      <main>
+      <main className='flex flex-col h-full'>
         <div className='px-5 py-4'>
           <h1 className='text-[22px] mb-4'>기록</h1>
           <MonthSelector />
         </div>
+        <DailyExpenseList dailyExpenses={dailyExpenses} />
       </main>
     </Layout>
   );

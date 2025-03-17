9d485a92d7f67a610f0f4be6e766f829c17f3a42
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-17 15:32:15 +0900

    fix: add return null, fix getDate call on DailyExpenseListItem

--- src/features/expense/ui/DailyExpenseListItem/DailyExpenseListItem.tsx
+++ src/features/expense/ui/DailyExpenseListItem/DailyExpenseListItem.tsx
@@ -4,11 +4,13 @@ import DailyExpenseRecord from '../DailyExpenseRecord/DailyExpenseRecord';
 
 const DailyExpenseListItem: React.FC<DailyExpense> = ({ date, expenses }) => {
   if (expenses.length === 0) {
-    return;
+    return null;
   }
 
   return (
-    <li aria-labelledby={`daily-expense-list-item-${date.getDate.toString()}`}>
+    <li
+      aria-labelledby={`daily-expense-list-item-${date.getDate().toString()}`}
+    >
       <p aria-label=''>{`${date.getDate().toString()}일`}</p>
       <ul>
         {expenses.map(({ id, category, memo, amount }: Expense) => (

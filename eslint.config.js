f461a83ec41a597d6254f6a25ec324f8e30ef2c0
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-18 20:17:42 +0900

    chore: add @tanstack/eslint-plugin-query and setup eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
+
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
@@ -30,6 +33,7 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@tanstack/query': pluginQuery,
       prettier,
     },
     rules: {

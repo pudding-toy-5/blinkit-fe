40b3a14c6fa50522ea67b5442e5a46ed28feaf29
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-18 23:00:32 +0900

    feat: add rendering tanstack query devtools in development

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDom from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import './index.css';
 
-import { routeTree } from './routeTree.gen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routeTree.gen';
+
+import './index.css';
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();
@@ -23,6 +26,7 @@ if (!rootElement.innerHTML) {
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
       </QueryClientProvider>
     </StrictMode>
   );

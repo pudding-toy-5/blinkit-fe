37c8bb46378504761f3bcb4971120bb05422a16d
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-18 17:07:47 +0900

    feat: add expense selectors

---
+++ src/features/expense/model/selectors.ts
@@ -0,0 +1,30 @@
+import useExpenseStore from './useExpenseStore';
+import useExpenseFormStore from './useExpenseStore';
+import useCategoryStore from './useCategoryStore';
+
+// expenseStore
+export const usePeriod = () => {
+  return useExpenseStore((state) => state.period);
+};
+
+export const useTotalAmount = () => {
+  return useExpenseStore((state) => state.totalAmount);
+};
+
+export const useExpenses = () => {
+  return useExpenseStore((state) => state.expenses);
+};
+
+export const useDailyExpenses = () => {
+  return useExpenseStore((state) => state.dailyExpenses);
+};
+
+// expenseFormStore
+export const useExpenseForm = () => {
+  return useExpenseFormStore((state) => state);
+};
+
+// categoryStore
+export const useCategories = () => {
+  return useCategoryStore((state) => state.categories);
+};

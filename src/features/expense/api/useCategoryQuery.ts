8cf932a37313990a25ec6febc035880fd7f07b1d
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-19 13:57:21 +0900

    refact: use useEntityQuery in category, expense query

--- src/features/expense/api/useCategoryQuery.ts
+++ src/features/expense/api/useCategoryQuery.ts
@@ -1,10 +1,8 @@
-import { useEffect } from 'react';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { createEntityHooks } from '@/features/common/useEntityQuery';
 
 import { Category } from '@/features/expense/model/types/Category';
 import useCategoryStore from '@/features/expense/model/useCategoryStore';
 
-import fakeApiCall from './fakeApiCall';
 import { queryKeys } from '../consts';
 
 const fetchCategories = async (): Promise<Category[]> => {
@@ -17,83 +15,22 @@ const fetchCategories = async (): Promise<Category[]> => {
   return res.json() as Promise<Category[]>;
 };
 
-const addCategory = async (category: Omit<Category, 'id'>) => {
-  return await fakeApiCall<Category>({
-    id: new Date().toString(),
-    ...category,
-  });
-};
+const setCategories = useCategoryStore((store) => store.setCategories);
 
-const updateCategory = async (category: Category) => {
-  return await fakeApiCall<Category>(category);
-};
+const {
+  useFetchEntities: useFetchEntities,
+  useAddEntity: useAddCategory,
+  useUpdateEntity: useUpdateCategory,
+  useDeleteEntity: useDeleteCategory,
+} = createEntityHooks<Category>(
+  queryKeys.categories,
+  fetchCategories,
+  setCategories
+);
 
-const deleteCategory = async (id: string) => {
-  return await fakeApiCall<string>(id);
-};
-
-export const useFetchCategories = () => {
-  const setCategories = useCategoryStore((state) => state.setCategories);
-  const { data, error, isLoading } = useQuery({
-    queryKey: [queryKeys.categories],
-    queryFn: fetchCategories,
-  });
-
-  useEffect(() => {
-    if (data) {
-      setCategories(data);
-    }
-  }, [data, setCategories]);
-
-  return { data, error, isLoading };
-};
-
-export const useAddCategory = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: addCategory,
-    onSuccess: (newCategory) => {
-      queryClient.setQueryData(
-        queryKeys.categories,
-        (oldData: Category[] | undefined) => {
-          return oldData ? [...oldData, newCategory] : [newCategory];
-        }
-      );
-    },
-  });
-};
-
-export const useUpdateCategory = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: updateCategory,
-    onSuccess: (updatedCategory) => {
-      queryClient.setQueryData(
-        queryKeys.categories,
-        (oldData: Category[] | undefined) => {
-          return oldData?.map((category) =>
-            category.id === updatedCategory.id ? updatedCategory : category
-          );
-        }
-      );
-    },
-  });
-};
-
-export const useDeleteCategory = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: deleteCategory,
-    onSuccess: (id) => {
-      queryClient.setQueryData(
-        queryKeys.categories,
-        (oldData: Category[] | undefined) => {
-          return oldData?.filter((category) => category.id !== id);
-        }
-      );
-    },
-  });
+export {
+  useFetchEntities,
+  useAddCategory,
+  useUpdateCategory,
+  useDeleteCategory,
 };
--- src/features/expense/api/useExpenseQuery.ts
+++ src/features/expense/api/useExpenseQuery.ts
@@ -1,11 +1,9 @@
-import { useEffect } from 'react';
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { createEntityHooks } from '@/features/common/useEntityQuery';
 
 import useExpenseStore from '@/features/expense/model/useExpenseStore';
 import { Expense } from '@/features/expense/model/types/Expense';
 
 import { queryKeys } from '../consts';
-import fakeApiCall from './fakeApiCall';
 
 const fetchExpenses = async (): Promise<Expense[]> => {
   const res = await fetch('./mock-expenses.json');
@@ -17,83 +15,13 @@ const fetchExpenses = async (): Promise<Expense[]> => {
   return res.json() as Promise<Expense[]>;
 };
 
-const addExpense = async (expense: Omit<Expense, 'id'>) => {
-  return await fakeApiCall<Expense>({
-    id: new Date().toString(),
-    ...expense,
-  });
-};
+const setExpenses = useExpenseStore((store) => store.setExpenses);
 
-const updateExpense = async (expense: Expense) => {
-  return await fakeApiCall<Expense>(expense);
-};
+const {
+  useFetchEntities: useFetchExpenses,
+  useAddEntity: useAddExpense,
+  useUpdateEntity: useUpdateExpense,
+  useDeleteEntity: useDeleteExpense,
+} = createEntityHooks<Expense>(queryKeys.expenses, fetchExpenses, setExpenses);
 
-const deleteExpense = async (id: string) => {
-  return await fakeApiCall<string>(id);
-};
-
-export const useFetchExpenses = () => {
-  const setExpenses = useExpenseStore((state) => state.setExpenses);
-  const { data, error, isLoading } = useQuery({
-    queryKey: [queryKeys.expenses],
-    queryFn: fetchExpenses,
-  });
-
-  useEffect(() => {
-    if (data) {
-      setExpenses(data);
-    }
-  }, [data, setExpenses]);
-
-  return { data, error, isLoading };
-};
-
-export const useAddExpense = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: addExpense,
-    onSuccess: (newExpense) => {
-      queryClient.setQueryData(
-        queryKeys.expenses,
-        (oldData: Expense[] | undefined) => {
-          return oldData ? [...oldData, newExpense] : [newExpense];
-        }
-      );
-    },
-  });
-};
-
-export const useUpdateExpense = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: updateExpense,
-    onSuccess: (updatedExpense) => {
-      queryClient.setQueryData(
-        queryKeys.expenses,
-        (oldData: Expense[] | undefined) => {
-          return oldData?.map((expense) =>
-            expense.id === updatedExpense.id ? updatedExpense : expense
-          );
-        }
-      );
-    },
-  });
-};
-
-export const useDeleteExpense = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: deleteExpense,
-    onSuccess: (id) => {
-      queryClient.setQueryData(
-        queryKeys.expenses,
-        (oldData: Expense[] | undefined) => {
-          return oldData?.filter((expense) => expense.id !== id);
-        }
-      );
-    },
-  });
-};
+export { useFetchExpenses, useAddExpense, useUpdateExpense, useDeleteExpense };

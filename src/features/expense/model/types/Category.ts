65b2d03e9c7e2ff15ffaac41a76f05f7c08d0fc2
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-19 15:23:40 +0900

    remove: remove State, Actions of Category

--- src/features/expense/model/types/Category.ts
+++ src/features/expense/model/types/Category.ts
@@ -1,15 +1,3 @@
-// categoryStore
-export interface CategoryState {
-  categories: Category[];
-}
-
-export interface CategoryActions {
-  setCategories: (categories: Category[]) => void;
-  addCategory: (category: Omit<Category, 'id'>) => void;
-  updateCategory: (category: Category) => void;
-  deleteCategory: (id: string) => void;
-}
-
 export interface Category {
   id: string;
   name: string;

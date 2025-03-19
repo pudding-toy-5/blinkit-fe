4c439d14d4d02a46fe15d9bcd8163dd35ced5964
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-19 15:23:15 +0900

    remove: remove expenseStore, categoryStore selectors

--- src/features/expense/model/selectors.ts
+++ src/features/expense/model/selectors.ts
@@ -1,30 +1,6 @@
-import useExpenseStore from './useExpenseStore';
 import useExpenseFormStore from './useExpenseFormStore';
-import useCategoryStore from './useCategoryStore';
-
-// expenseStore
-export const usePeriod = () => {
-  return useExpenseStore((state) => state.period);
-};
-
-export const useTotalAmount = () => {
-  return useExpenseStore((state) => state.totalAmount);
-};
-
-export const useExpenses = () => {
-  return useExpenseStore((state) => state.expenses);
-};
-
-export const useDailyExpenses = () => {
-  return useExpenseStore((state) => state.dailyExpenses);
-};
 
 // expenseFormStore
 export const useExpenseForm = () => {
   return useExpenseFormStore((state) => state);
-};
-
-// categoryStore
-export const useCategories = () => {
-  return useCategoryStore((state) => state.categories);
 };

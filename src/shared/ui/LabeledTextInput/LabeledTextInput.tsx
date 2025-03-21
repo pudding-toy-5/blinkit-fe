8f91604a57e5b80c3a7df74af7f66937c0ebcb77
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-22 00:35:00 +0900

    feat: add state to LabeledTextInput

--- src/shared/ui/LabeledTextInput/LabeledTextInput.tsx
+++ src/shared/ui/LabeledTextInput/LabeledTextInput.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+import { cn } from '@/shared/ui/styles/utils';
+
 export interface LabeledTextInputProps {
   label: string;
   id: string;
@@ -10,6 +12,7 @@ export interface LabeledTextInputProps {
   value?: string;
   onChange?: (newValue: string) => void;
   maxLength?: number;
+  state?: 'completed' | 'disabled' | 'error';
 }
 
 const LabeledTextInput: React.FC<LabeledTextInputProps> = ({
@@ -19,12 +22,13 @@ const LabeledTextInput: React.FC<LabeledTextInputProps> = ({
   value,
   onChange,
   maxLength,
+  state,
 }) => {
   const inputRef = React.useRef(null);
 
   return (
     <div className='flex flex-col items-center gap-1.5'>
-      <Label className='mr-auto text-base' htmlFor={id}>
+      <Label className='mr-auto text-base text-[#222]' htmlFor={id}>
         {label}
       </Label>
       <Input
@@ -34,20 +38,35 @@ const LabeledTextInput: React.FC<LabeledTextInputProps> = ({
         placeholder={placeholder}
         value={value}
         maxLength={maxLength}
+        disabled={state && state === 'disabled'}
         onChange={(e) => {
           if (onChange) {
             onChange(e.target.value);
           }
         }}
+        className={cn(
+          'text-base text-[#999] border-[#999] ',
+          'focus:border-[#222] focus:outlined-none focus:ring-0 focus:text-[#222]',
+          state && state === 'completed' && 'border-[#28a745]',
+          state && state === 'disabled' && 'border-[#ccc]',
+          state && state === 'error' && 'border-[#d32f2f]'
+        )}
       />
       {maxLength && value !== undefined && (
-        <p
-          aria-label={`${id}-length-counter`}
-          id={`${id}-length-counter`}
-          className='ml-auto'
-        >
-          {value.length}/{maxLength}
-        </p>
+        <span aria-live='polite' className={cn('flex flex-row ml-auto')}>
+          <p
+            className={cn(
+              'text-[#222]',
+              state &&
+                state === 'error' &&
+                value.length > maxLength &&
+                'text-[#d32f2f]'
+            )}
+          >
+            {value.length}
+          </p>
+          <p className='text-[#999]'>/{maxLength}</p>
+        </span>
       )}
     </div>
   );

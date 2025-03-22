fa255755ab8fdb85ba7f4c37feaa5d445cc2cced
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-22 13:02:25 +0900

    feat: add LabeledTextInput to component-test page

--- src/app/routes/component-test.tsx
+++ src/app/routes/component-test.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { createFileRoute } from '@tanstack/react-router';
 
 import Layout from '@/shared/ui/layout/Layout';
@@ -8,9 +10,30 @@ export const Route = createFileRoute('/component-test')({
 });
 
 function RouteComponent() {
+  const [value, setValue] = React.useState<string>('');
+
+  const handleChange = (newValue: string) => setValue(newValue);
+
   return (
     <Layout>
-      <LabeledTextInput label='component-test' />
+      <LabeledTextInput
+        label='1 default'
+        placeholder='placeholder'
+        id='id'
+        value={value}
+        onChange={handleChange}
+        maxLength={20}
+      />
+      <LabeledTextInput label='2 focused' id='focused' />
+      <LabeledTextInput label='3 completed' id='completed' state='completed' />
+      <LabeledTextInput label='4 disabled' id='disabled' state='disabled' />
+      <LabeledTextInput
+        label='5 error'
+        id='error'
+        state='error'
+        value={'333'}
+        maxLength={1}
+      />
     </Layout>
   );
 }

352965dcd60c6edbf08647eef65425da264e3aa1
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-23 18:22:56 +0900

    refact: update useTotalAmount hook in TotalAmount

--- src/features/expense/ui/TotalAmount/TotalAmount.tsx
+++ src/features/expense/ui/TotalAmount/TotalAmount.tsx
@@ -1,13 +1,13 @@
-import { useExpenseStore } from '@/features/expense/model/useExpenseStore';
+import { useTotalAmount } from '@/features/expense/api/useExpenseQuery';
 
 const TotalAmount: React.FC = () => {
-  const totalAmount = useExpenseStore((state) => state.totalAmount);
+  const { totalAmount } = useTotalAmount();
 
   return (
     <div className='flex flex-col'>
       <p>총 소비 내역</p>
       <div>
-        <p>{totalAmount}원</p>
+        <p>{totalAmount.toString()}원</p>
       </div>
     </div>
   );

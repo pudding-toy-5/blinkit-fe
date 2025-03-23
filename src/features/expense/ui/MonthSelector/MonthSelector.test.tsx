b370a73b805b0b2f1cfddfd55765c560ea672da8
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-23 18:21:17 +0900

    remove: remove MonthSelector test

--- src/features/expense/ui/MonthSelector/MonthSelector.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, it } from 'vitest';
-
-describe('MonthSelector', () => {
-  it('renders previous button.', () => {});
-  it('renders current button.', () => {});
-  it('when current button is clicked, renders select month drawer.', () => {});
-  it('renders next button.', () => {});
-});

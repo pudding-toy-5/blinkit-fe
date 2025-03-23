42009a7b5e3395d0e169c7b4dd0ae562a069e4cb
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-23 17:31:18 +0900

    style: update Layout style

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,6 +1,6 @@
 const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
-    <div className='max-w-sm mx-auto h-screen bg-white flex flex-col'>
+    <div className='max-w-sm mx-auto min-h-screen bg-white flex flex-col'>
       {children}
     </div>
   );

82374054920417618c368eb0126ff756d0101256
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-24 16:22:14 +0900

    test: add onClick test to SignInButton test

--- src/features/sign-in/ui/SignInButton/SignInButton.test.tsx
+++ src/features/sign-in/ui/SignInButton/SignInButton.test.tsx
@@ -1,20 +1,39 @@
-import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+import { fireEvent, render } from '@testing-library/react';
 
 import SignInButton from './SignInButton';
 
 describe('SignInButton', () => {
   it('when service is google, renders google text and style.', () => {
-    const { getByText } = render(<SignInButton service='google' />);
+    const onClick = vi.fn();
+    const { getByText } = render(
+      <SignInButton service='google' onClick={onClick} />
+    );
     const button = getByText('구글 계정으로 시작하기');
 
     expect(button).toBeInTheDocument();
   });
 
   it('when service is naver, renders naver text and style.', () => {
-    const { getByText } = render(<SignInButton service='naver' />);
+    const onClick = vi.fn();
+    const { getByText } = render(
+      <SignInButton service='naver' onClick={onClick} />
+    );
     const button = getByText('네이버 계정으로 시작하기');
 
     expect(button).toBeInTheDocument();
+  });
+
+  it('when button is clicked, onClick is called.', () => {
+    const onClick = vi.fn();
+    const { getByText } = render(
+      <SignInButton service='naver' onClick={onClick} />
+    );
+    const button = getByText('네이버 계정으로 시작하기');
+
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    expect(onClick).toBeCalledTimes(1);
   });
 });

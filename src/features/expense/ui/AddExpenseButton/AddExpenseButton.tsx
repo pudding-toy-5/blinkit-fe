3590ee149b27c8e45354eee580b7f330398ed667
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-24 22:01:52 +0900

    feat: add role to AddExpenseButton

--- src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
+++ src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
@@ -14,6 +14,7 @@ const AddExpenseButton: React.FC<AddExpenseButtonProps> = ({ onClick }) => {
       aria-label='지출 추가'
       className='absolute right-[20px] bottom-[32px] w-14 h-14 p-4 rounded-[28px] bg-[#89f336] hover:bg-[#89f336] hover:ring'
       asChild
+      role='button'
     >
       <Plus color='#222222' size='24' />
     </Button>

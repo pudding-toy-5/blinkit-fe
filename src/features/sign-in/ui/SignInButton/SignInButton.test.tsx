2e0e463b654bfbfb6cbe0bb32eea478a5b2c6d44
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-24 16:35:26 +0900

    test: change getByText to Role in SignInButton

--- src/features/sign-in/ui/SignInButton/SignInButton.test.tsx
+++ src/features/sign-in/ui/SignInButton/SignInButton.test.tsx
@@ -6,30 +6,32 @@ import SignInButton from './SignInButton';
 describe('SignInButton', () => {
   it('when service is google, renders google text and style.', () => {
     const onClick = vi.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <SignInButton service='google' onClick={onClick} />
     );
-    const button = getByText('구글 계정으로 시작하기');
+    const button = getByRole('button');
 
     expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('구글 계정으로 시작하기');
   });
 
   it('when service is naver, renders naver text and style.', () => {
     const onClick = vi.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <SignInButton service='naver' onClick={onClick} />
     );
-    const button = getByText('네이버 계정으로 시작하기');
+    const button = getByRole('button');
 
     expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent('네이버 계정으로 시작하기');
   });
 
   it('when button is clicked, onClick is called.', () => {
     const onClick = vi.fn();
-    const { getByText } = render(
+    const { getByRole } = render(
       <SignInButton service='naver' onClick={onClick} />
     );
-    const button = getByText('네이버 계정으로 시작하기');
+    const button = getByRole('button');
 
     expect(button).toBeInTheDocument();
     fireEvent.click(button);

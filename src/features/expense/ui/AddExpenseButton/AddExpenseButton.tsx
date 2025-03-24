d6b665debc7a1de31eb536779a637b64af6ce9f3
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-24 22:09:22 +0900

    refact: replace Button to div, remove variant, asChild

--- src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
+++ src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
@@ -1,23 +1,19 @@
 import { Plus } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
-
 interface AddExpenseButtonProps {
   onClick: () => void;
 }
 
 const AddExpenseButton: React.FC<AddExpenseButtonProps> = ({ onClick }) => {
   return (
-    <Button
+    <div
       onClick={onClick}
-      variant='outline'
       aria-label='지출 추가'
-      className='absolute right-[20px] bottom-[32px] w-14 h-14 p-4 rounded-[28px] bg-[#89f336] hover:bg-[#89f336] hover:ring'
-      asChild
+      className='absolute right-[20px] bottom-[32px] w-14 h-14 p-4 rounded-[28px] bg-[#89f336] hover:ring'
       role='button'
     >
       <Plus color='#222222' size='24' />
-    </Button>
+    </div>
   );
 };
 

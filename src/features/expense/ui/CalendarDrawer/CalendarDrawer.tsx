f8b959996fefe6efd8fa15cfb1951c2cee6bfb64
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-25 16:52:52 +0900

    feat: add triggerText and onClickToday to CalendarDrawer

--- src/features/expense/ui/CalendarDrawer/CalendarDrawer.tsx
+++ src/features/expense/ui/CalendarDrawer/CalendarDrawer.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
+import { addMonths } from 'date-fns';
 import { ko } from 'date-fns/locale';
+
 import { X } from 'lucide-react';
 
 import {
@@ -16,20 +20,31 @@ import { Calendar } from '@/components/ui/calendar';
 import SubmitButton from '@/shared/ui/SubmitButton';
 
 export interface CalendarDrawerProps {
+  triggerText: string;
   date: Date | undefined;
   setDate: (newDate: Date | undefined) => void;
 }
 
-const CalendarDrawer: React.FC<CalendarDrawerProps> = ({ date, setDate }) => {
+const CalendarDrawer: React.FC<CalendarDrawerProps> = ({
+  triggerText,
+  date,
+  setDate,
+}) => {
+  const _today = new Date();
+  const today = new Date(
+    _today.getFullYear(),
+    _today.getMonth(),
+    _today.getDate()
+  );
   return (
     <Drawer>
-      <DrawerTrigger>Open</DrawerTrigger>
+      <DrawerTrigger>{triggerText}</DrawerTrigger>
       <DrawerContent className='w-full max-w-sm mx-auto p-6'>
         <DrawerHeader className='flex flex-row items-center justify-between rounded-t-full p-0'>
           <div className='flex-1' />
           <DrawerTitle>날짜 선택</DrawerTitle>
           <DrawerClose className='flex-1 flex justify-end'>
-            <div role='button'>
+            <div role='button' aria-label='close button'>
               <X />
             </div>
           </DrawerClose>
@@ -40,9 +55,11 @@ const CalendarDrawer: React.FC<CalendarDrawerProps> = ({ date, setDate }) => {
             selected={date}
             onSelect={setDate}
             locale={ko}
+            month={date}
+            onMonthChange={setDate}
             className='px-auto py-0'
             classNames={{
-              caption_label: '17px',
+              caption_label: 'font-[17px]',
               head_cell: 'w-[44px] font-normal',
               row: 'flex w-full mt-0',
               day: 'size-[44px]',
@@ -52,10 +69,16 @@ const CalendarDrawer: React.FC<CalendarDrawerProps> = ({ date, setDate }) => {
             }}
           />
           <DrawerFooter className='flex flex-row flex-center pb-0 pt-4 px-auto'>
-            <Button className='font-[15px] h-13' variant='ghost'>
+            <Button
+              className='font-[15px] h-13'
+              variant='ghost'
+              onClick={() => {
+                setDate(today);
+              }}
+            >
               오늘
             </Button>
-            <DrawerClose asChild className='flex-1'>
+            <DrawerClose asChild className='flex-1' aria-label='날짜 선택 버튼'>
               <SubmitButton
                 text='선택'
                 state='default'

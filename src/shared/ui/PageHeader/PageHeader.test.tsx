a5d46d03fdc91a61ec0607d9b9c9b22f3fdfbd44
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-25 18:04:00 +0900

    test: add PageHeader test

---
+++ src/shared/ui/PageHeader/PageHeader.test.tsx
@@ -0,0 +1,20 @@
+import { describe, it } from 'vitest';
+import { render } from '@testing-library/react';
+
+import PageHeader from './PageHeader';
+
+describe('PageHeader', () => {
+  it('renders Logo.', () => {
+    const { getByLabelText } = render(<PageHeader />);
+    const logo = getByLabelText('로고');
+
+    expect(logo).toBeInTheDocument();
+  });
+
+  it('renders Setting Button.', () => {
+    const { getByLabelText } = render(<PageHeader />);
+    const settingButton = getByLabelText('세팅 버튼');
+
+    expect(settingButton).toBeInTheDocument();
+  });
+});

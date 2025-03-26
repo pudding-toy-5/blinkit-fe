65dc58b5d5e5cfc257686bed56b9b825f339284f
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-26 17:11:03 +0900

    style: add border top radius to SelectMonthDrawer

--- src/features/expense/ui/SelectMonthDrawer/SelectMonthDrawer.tsx
+++ src/features/expense/ui/SelectMonthDrawer/SelectMonthDrawer.tsx
@@ -11,6 +11,7 @@ import {
 
 import Period from '@/features/expense/model/types/Period';
 import SelectMonthList from './SelectMonthList';
+
 export interface SelectMonthDrawerProps {
   trigger: React.ReactNode;
   period: Period;
@@ -25,7 +26,10 @@ const SelectMonthDrawer: React.FC<SelectMonthDrawerProps> = ({
   return (
     <Drawer>
       <DrawerTrigger>{trigger}</DrawerTrigger>
-      <DrawerContent className='w-full max-w-sm mx-auto py-6 px-5'>
+      <DrawerContent
+        className='w-full max-w-sm mx-auto py-6 px-5'
+        style={{ borderRadius: '20px 20px 0 0 ' }}
+      >
         <DrawerHeader className='flex flex-row items-center justify-between p-0'>
           <div className='flex-1' />
           <DrawerTitle className='font-[17px]'>조회 월 선택</DrawerTitle>

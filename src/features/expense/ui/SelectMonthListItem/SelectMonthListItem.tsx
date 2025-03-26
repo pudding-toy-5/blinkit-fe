89c0aac2c029a71747d2902ccbca15c0e2834552
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-26 20:28:28 +0900

    feat: add key to list item and fix svg rules

--- src/features/expense/ui/SelectMonthListItem/SelectMonthListItem.tsx
+++ src/features/expense/ui/SelectMonthListItem/SelectMonthListItem.tsx
@@ -16,7 +16,7 @@ const SelectMonthListItem: React.FC<SelectMonthListItemProps> = ({
   handleClick,
 }) => {
   return (
-    <li className='w-full h-6'>
+    <li className='w-full h-6' key={`${year.toString()}-${month.toString()}`}>
       <Button
         variant='ghost'
         className='flex flex-row w-full h-full p-0 font-[15px] items-center'
@@ -36,8 +36,8 @@ const SelectMonthListItem: React.FC<SelectMonthListItemProps> = ({
               fill='none'
             >
               <path
-                fill-rule='evenodd'
-                clip-rule='evenodd'
+                fillRule='evenodd'
+                clipRule='evenodd'
                 d='M21 7.28599L10.4319 17.373C10.2483 17.5483 9.99731 17.6471 9.73541 17.6471C9.4735 17.6471 9.22256 17.5483 9.03891 17.373L3 11.609L4.39299 10.323L9.73541 15.4223L19.607 6L21 7.28599Z'
                 fill='#28A745'
               />

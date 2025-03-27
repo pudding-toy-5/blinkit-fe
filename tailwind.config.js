b1d2bcc30f3011cdd9ab964afeda99e95a4d96b9
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-27 14:53:42 +0900

    chore: add pretendard to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };

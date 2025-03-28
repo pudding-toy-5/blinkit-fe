31ecbe01cfef0327bec84b97cfd081331b5af439
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-28 18:50:02 +0900

    fix: fix RightArrow to ArrowRight

--- src/shared/ui/icons/ArrowRight.tsx
+++ src/shared/ui/icons/ArrowRight.tsx
@@ -1,6 +1,6 @@
 import IconProps from './types';
 
-const RightArrow: React.FC<IconProps> = ({ size = 16 }) => {
+const ArrowRight: React.FC<IconProps> = ({ size = 16 }) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -18,4 +18,4 @@ const RightArrow: React.FC<IconProps> = ({ size = 16 }) => {
   );
 };
 
-export default RightArrow;
+export default ArrowRight;

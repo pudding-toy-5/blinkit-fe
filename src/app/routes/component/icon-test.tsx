954488d27452c42dd4b870eeab5d75fba0bef26e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-28 18:26:24 +0900

    feat: add icon-test to component route

--- src/app/routes/component/icon-test.tsx
+++ src/app/routes/component/icon-test.tsx
@@ -2,7 +2,14 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import Layout from '@/shared/ui/layout/Layout';
 
+import ArrowLeft from '@/shared/ui/icons/ArrowLeft';
+import ArrowLeftFilled from '@/shared/ui/icons/ArrowLeftFilled';
+import Check from '@/shared/ui/icons/Check';
+import FilledExclamation from '@/shared/ui/icons/FilledExclamation';
+import FilledSuccess from '@/shared/ui/icons/FilledSuccess';
+import ArrowRight from '@/shared/ui/icons/ArrowRight';
 import Setting from '@/shared/ui/icons/Setting';
+import X from '@/shared/ui/icons/X';
 
 export const Route = createFileRoute('/component/icon-test')({
   component: RouteComponent,
@@ -15,7 +22,42 @@ function RouteComponent() {
     <Layout>
       <div className='flex flex-row'>
         {sizes.map((size) => (
+          <ArrowLeft key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <ArrowLeftFilled key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <ArrowRight key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <Check key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <FilledExclamation key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <FilledSuccess key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
           <Setting key={size} size={size} />
+        ))}
+      </div>
+      <div className='flex flex-row'>
+        {sizes.map((size) => (
+          <X key={size} size={size} />
         ))}
       </div>
     </Layout>

0f1087801d5f4bcb2b9501c6141794340e02fe27
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-28 14:57:00 +0900

    feat: add toast function to onClick in buttons of toaster route

--- src/app/routes/component/toaster.tsx
+++ src/app/routes/component/toaster.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { toast } from 'sonner';
 
 import Layout from '@/shared/ui/layout/Layout';
+
 import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/component/toaster')({
@@ -12,10 +13,10 @@ export const Route = createFileRoute('/component/toaster')({
 function RouteComponent() {
   return (
     <Layout>
-      <Button onClick={() => toast('Event has been created', {})}>
-        error toast
+      <Button onClick={() => toast.success('Event has been created')}>
+        success toast
       </Button>
-      <Button>success toast</Button>
+      <Button onClick={() => toast.error('message')}>error toast</Button>
     </Layout>
   );
 }

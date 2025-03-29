1ce504024689a9f4d86366f145a6ec62212bdbbc
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-29 10:59:41 +0900

    Revert "feat: add layout to __root route"
    This reverts commit 48f2a8f362e8cd0952fc57ba16f637dc569b3609.

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,14 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import Layout from '@/shared/ui/layout/Layout';
-
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Layout>
-        <Outlet />
-      </Layout>
+      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),

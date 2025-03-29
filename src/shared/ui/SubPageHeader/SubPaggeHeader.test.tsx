3bcdde2b3c5efa450e70b6cf34cba3ea5687a78e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-29 11:33:23 +0900

    test: add SubPageHeader test

--- src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
+++ src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
@@ -1,34 +1,88 @@
-import { describe, it, expect } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { fireEvent, render } from '@testing-library/react';
 
+// mock react-router useRouter().history.back()
+const mockBack = vi.fn();
+
+vi.mock('@tanstack/react-router', () => ({
+  useRouter: () => ({
+    history: {
+      back: mockBack,
+    },
+  }),
+}));
+
+// after mock function
 import SubPageHeader, { SubPageHeaderProps } from './SubPageHeader';
 
 describe('SubPageHeader', () => {
-  const renderSubPageHeader = ({ backLink, title }: SubPageHeaderProps) =>
-    render(<SubPageHeader backLink={backLink} title={title} />);
+  const props: SubPageHeaderProps = {
+    title: 'sub page title',
+    back: undefined,
+    close: undefined,
+    onClose: undefined,
+  };
+
+  const renderSubPageHeader = ({
+    title,
+    back,
+    close,
+    onClose,
+  }: SubPageHeaderProps) =>
+    render(
+      <SubPageHeader
+        title={title}
+        back={back}
+        close={close}
+        onClose={onClose}
+      />
+    );
 
   it('renders the provided title correctly.', () => {
-    const props: SubPageHeaderProps = {
-      backLink: '/',
-      title: 'title',
-    };
-    const { getByText } = renderSubPageHeader(props);
+    const { getByText } = renderSubPageHeader({ ...props });
+    const title = getByText(props.title);
 
-    const titleElement = getByText('title');
-    expect(titleElement).toBeInTheDocument();
-    expect(titleElement.tagName.toLowerCase()).toBe('h1');
+    expect(title).toBeInTheDocument();
+    expect(title.tagName.toLowerCase()).toBe('h1');
   });
 
-  it(`renders Link with correct aria-label and to attribute(backLink).`, () => {
-    const props: SubPageHeaderProps = {
-      backLink: '/backLink',
-      title: '',
-    };
-    const { getByRole } = renderSubPageHeader(props);
-    const link = getByRole('link', { name: '뒤로가기' });
+  it('renders provided back button, when clicks, calls router.history.back().', () => {
+    const { getByLabelText } = renderSubPageHeader({
+      ...props,
+      back: true,
+    });
 
-    expect(link).not.toBeNull();
-    expect(link).toHaveAttribute('href', props.backLink);
-    expect(link).toHaveAttribute('aria-label', '뒤로가기');
+    const backButton = getByLabelText('back button');
+    expect(backButton).toBeInTheDocument();
+    expect(backButton).toHaveAttribute('role', 'button');
+
+    fireEvent.click(backButton);
+    expect(mockBack).toBeCalledTimes(1);
+  });
+
+  it('renders provided close button.', () => {
+    const { getByLabelText } = renderSubPageHeader({
+      ...props,
+      close: true,
+    });
+
+    const closeButton = getByLabelText('close button');
+    expect(closeButton).toBeInTheDocument();
+    expect(closeButton).toHaveAttribute('role', 'button');
+  });
+
+  it('renders provided onClose with close.', () => {
+    const onClose = vi.fn();
+    const { getByLabelText } = renderSubPageHeader({
+      ...props,
+      close: true,
+      onClose: onClose,
+    });
+
+    const closeButton = getByLabelText('close button');
+    expect(closeButton).toBeInTheDocument();
+
+    fireEvent.click(closeButton);
+    expect(onClose).toBeCalledTimes(1);
   });
 });

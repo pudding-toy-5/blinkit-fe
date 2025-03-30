8e2b89e907a715977465c2a72b409809f98842e9
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-30 14:43:12 +0900

    feat: add size, onDelete to CategoryTag

--- src/features/category/ui/CategoryTag/CategoryTag.tsx
+++ src/features/category/ui/CategoryTag/CategoryTag.tsx
@@ -1,14 +1,43 @@
+import { Button } from '@/components/ui/button';
+import X from '@/shared/ui/icons/X';
+import { cn } from '@/shared/ui/styles/utils';
+
 export interface CategoryTagProps {
   tagName: string;
+  size?: 'small' | 'medium' | 'large';
+  onDelete?: () => void;
 }
 
-const CategoryTag: React.FC<CategoryTagProps> = ({ tagName }) => {
+const CategoryTag: React.FC<CategoryTagProps> = ({
+  tagName,
+  size,
+  onDelete,
+}) => {
   return (
     <div
-      className='bg-[#EAF6EC] text-[#28A745] rounded-full px-2 py-1 text-[13px]'
+      className={cn(
+        'bg-[#EAF6EC] text-[#28A745] rounded-full text-[13px]',
+        'px-2 py-1',
+        size === 'small' && 'px-2 py-1',
+        size === 'medium' && 'px-3 py-2',
+        size === 'large' && 'px-4 py-3'
+      )}
       aria-label={`카테고리: ${tagName}`}
     >
       <p>{tagName}</p>
+      {onDelete && (
+        <Button
+          className='ml-1 w-3 h-3'
+          size='icon'
+          variant='ghost'
+          aria-label={`category tag - ${tagName} - delete button`}
+          onClick={() => {
+            onDelete();
+          }}
+        >
+          <X size={12} />
+        </Button>
+      )}
     </div>
   );
 };

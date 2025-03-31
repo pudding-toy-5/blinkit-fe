be5ecb47e301c5e80565d087af3418ef60e82715
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-31 21:50:02 +0900

    lint: lint src/app/main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react';
-import ReactDom from 'react-dom/client';
+import 'pretendard/dist/web/static/pretendard.css';
+import './index.css';
 
 // tanstack-query
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 // tanstack-router
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { StrictMode } from 'react';
+import ReactDom from 'react-dom/client';
 
 import Toaster from '@/shared/ui/Toaster';
 
-import 'pretendard/dist/web/static/pretendard.css';
-import './index.css';
+import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();

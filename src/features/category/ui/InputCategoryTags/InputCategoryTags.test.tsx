a731bed2ea810adaa14456db7280dbd4abf1c9c2
Dokyun Claire Lim <im.koremp@gmail.com>
2025-03-31 15:06:59 +0900

    test: update test name, add check error on InputCategoryTags

--- src/features/category/ui/InputCategoryTags/InputCategoryTags.test.tsx
+++ src/features/category/ui/InputCategoryTags/InputCategoryTags.test.tsx
@@ -21,71 +21,88 @@ describe('InputCategoryTags', () => {
       />
     );
 
-  it('renders input tag with correct aria-label.', () => {
-    const placeholder = 'placeholder-test-text';
-    const { getByLabelText } = renderElement({ placeholder: placeholder });
-    const input = getByLabelText('카테고리 추가 입력');
-    expect(input).toBeInTheDocument();
-    expect(input).toHaveAttribute('placeholder', placeholder);
-    expect(input.tagName.toLowerCase()).toBe('input');
-  });
-
-  it('when value has more than one item, renders CategoryTags with value item.', () => {
-    const values = ['first', 'second', 'third'];
-    const { getByText } = renderElement({ value: values });
-    const items = values.map((item) => getByText(item));
-
-    items.forEach((item) => {
-      expect(item).toBeInTheDocument();
-    });
-  });
-
-  it('when CategoryTag delete button is clicked, calls onChange with removed values, removes clicked CategoryTag', () => {
-    const first = 'first';
-    const values: string[] = [first, 'second', 'third'];
-    const onChange = vi.fn();
-    const { getByText } = renderElement({
-      value: values,
-      onChange,
-    });
-    const firstItem = getByText(first);
-
-    expect(firstItem).toBeInTheDocument();
-    expect(firstItem.firstElementChild).not.toBeNull();
-
-    if (firstItem.firstElementChild !== null) {
-      const deleteButton = firstItem.firstElementChild;
-      expect(deleteButton.tagName.toLowerCase()).toBe('button');
-
-      fireEvent.click(deleteButton);
-
-      expect(onChange).toBeCalledWith(values.filter((item) => item !== first));
-    }
-  });
-
-  describe('input placeholder', () => {
-    it('when value is empty, renders input tag with placeholder.', () => {
-      const placeholder = 'placeholder-test-text';
-      const { getByPlaceholderText } = renderElement({
-        placeholder: placeholder,
-      });
-      const input = getByPlaceholderText(placeholder);
-
+  describe('input', () => {
+    it('renders input tag with aria-label.', () => {
+      const { getByLabelText } = renderElement({});
+      const input = getByLabelText('카테고리 추가 입력');
       expect(input).toBeInTheDocument();
       expect(input.tagName.toLowerCase()).toBe('input');
     });
 
-    it('when value is not empty, renders input tag without placeholder.', () => {
-      const { getByRole } = renderElement({ value: ['not-empty'] });
+    it('disabled input when length of value is over 3.', () => {
+      const values: string[] = ['11', '22', '33'];
+      const { getByRole } = renderElement({ value: values });
       const input = getByRole('textbox');
-
       expect(input).toBeInTheDocument();
-      expect(input).not.toHaveAttribute('placeholder');
+      expect(input).toBeDisabled();
+    });
+
+    describe('placeholder', () => {
+      it('when value is empty, renders input tag with placeholder.', () => {
+        const placeholder = 'placeholder-test-text';
+        const { getByPlaceholderText } = renderElement({
+          value: [],
+          placeholder: placeholder,
+        });
+        const input = getByPlaceholderText(placeholder);
+
+        expect(input).toBeInTheDocument();
+        expect(input.tagName.toLowerCase()).toBe('input');
+      });
+
+      it('when value is not empty, renders input tag without placeholder.', () => {
+        const { getByRole } = renderElement({ value: ['not-empty'] });
+        const input = getByRole('textbox');
+
+        expect(input).toBeInTheDocument();
+        expect(input).not.toHaveAttribute('placeholder');
+      });
+    });
+  });
+
+  it('throw error when value.length > 3.', () => {
+    expect(() => renderElement({ value: ['1', '2', '3', '4'] })).toThrow(Error);
+  });
+
+  describe('CategoryTags', () => {
+    it('renders CategoryTags with value.', () => {
+      const values = ['first', 'second', 'third'];
+      const { getByText } = renderElement({ value: values });
+      const items = values.map((item) => getByText(item));
+
+      items.forEach((item) => {
+        expect(item).toBeInTheDocument();
+      });
+    });
+
+    it('calls onChange when clicks delete-button of CategoryTag', () => {
+      const first = 'first';
+      const values: string[] = [first, 'second', 'third'];
+      const onChange = vi.fn();
+      const { getByText } = renderElement({
+        value: values,
+        onChange,
+      });
+      const firstItem = getByText(first);
+
+      expect(firstItem).toBeInTheDocument();
+      expect(firstItem.firstElementChild).not.toBeNull();
+
+      if (firstItem.firstElementChild !== null) {
+        const deleteButton = firstItem.firstElementChild;
+        expect(deleteButton.tagName.toLowerCase()).toBe('button');
+
+        fireEvent.click(deleteButton);
+
+        expect(onChange).toBeCalledWith(
+          values.filter((item) => item !== first)
+        );
+      }
     });
   });
 
   describe('add-button', () => {
-    it('renders add-button with correct aria-label attribute.', () => {
+    it('renders add-button with aria-label.', () => {
       const { getByRole } = renderElement({});
       const addButton = getByRole('button', { name: /추가/i });
 
@@ -96,7 +113,7 @@ describe('InputCategoryTags', () => {
       );
     });
 
-    it('when input is empty, add-button is disabled.', () => {
+    it('disables when input is empty.', () => {
       const { getByRole } = renderElement({});
       const addButton = getByRole('button', { name: /추가/i });
       const input = getByRole('textbox');
@@ -105,7 +122,7 @@ describe('InputCategoryTags', () => {
       expect(addButton).toBeDisabled();
     });
 
-    it('when add-button is clicked, calls onChange with input value, changes input value to empty.', () => {
+    it('calls onChange with input value when button is clicked.', () => {
       const onChange = vi.fn();
       const placeholder = 'placeholder-test';
       const inputValue = 'input-value-test';

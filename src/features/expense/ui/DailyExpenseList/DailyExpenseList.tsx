034690234dcce92d881cb817c9638318d4dfc783
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-01 15:50:49 +0900

    feat; add onScroll to DailyExpenseList props

--- src/features/expense/ui/DailyExpenseList/DailyExpenseList.tsx
+++ src/features/expense/ui/DailyExpenseList/DailyExpenseList.tsx
@@ -4,16 +4,19 @@ import { cn } from '@/shared/ui/styles/utils';
 
 export interface DailyExpenseListProps {
   dailyExpenses: DailyExpense[];
+  onScroll: (e: React.UIEvent<HTMLUListElement>) => void;
 }
 
 const DailyExpenseList: React.FC<DailyExpenseListProps> = ({
   dailyExpenses,
+  onScroll,
 }) => {
   return (
     <ul
       className={cn(
         'flex flex-1 flex-col gap-6 scroll list-none overflow-y-auto justify-start py-6 pl-5 pr-4 bg-[#F5F3F0]'
       )}
+      onScroll={onScroll}
       aria-label='daily-expense-list'
     >
       {dailyExpenses.length === 0 ? (

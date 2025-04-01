ce48d8049573b1b2eb596fbe6c6283bc7069cae8
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-01 18:47:36 +0900

    rename: rename about to expenses.new route

--- src/app/routes/expenses.new.tsx
+++ src/app/routes/expenses.new.tsx
@@ -1,18 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import SubPageHeader from '@/shared/ui/SubPageHeader';
 import ExpenseForm from '@/features/expense/ui/ExpenseForm';
+import Layout from '@/shared/ui/layout/Layout';
+import SubPageHeader from '@/shared/ui/SubPageHeader';
 
-export const Route = createFileRoute('/about')({
+export const Route = createFileRoute('/expenses/new')({
   component: AboutPage,
 });
 
 export function AboutPage() {
   return (
-    <div className='max-w-sm mx-auto h-screen bg-gray-100 flex flex-col'>
-      <SubPageHeader backLink='/' title='About Page' />
+    <Layout>
+      <SubPageHeader title='지출 내역 추가' back />
       <ExpenseForm />
-    </div>
+    </Layout>
   );
 }
 

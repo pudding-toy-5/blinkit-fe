0f83181ed314bd66a0c7c76f9b778065d04bd9e2
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-02 19:51:36 +0900

    test: add QueryClientProvider to ExpenseForm

--- src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
+++ src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 import { describe, it } from 'vitest';
 
 import ExpenseForm, { ExpenseFormProps } from './ExpenseForm';
 
 describe('ExpenseForm', () => {
-  const renderElement = ({
-    submitButtonText = 'submit button text',
-  }: ExpenseFormProps) =>
-    render(<ExpenseForm submitButtonText={submitButtonText} />);
+  const testQueryClient = new QueryClient();
+
+  const renderElement = ({ expense = undefined }: ExpenseFormProps) =>
+    render(
+      <QueryClientProvider client={testQueryClient}>
+        <ExpenseForm expense={expense} />
+      </QueryClientProvider>
+    );
 
   describe('date', () => {
     it('renders label.', () => {
@@ -48,11 +53,25 @@ describe('ExpenseForm', () => {
     });
   });
 
-  it('renders provided submitButtonText.', () => {
-    const text = 'submit button text example';
-    const { getByRole } = renderElement({ submitButtonText: text });
-    const submitButton = getByRole('button', { name: text });
+  describe('submit button', () => {
+    it('renders 추가 when expense is not provided.', () => {
+      const { getByRole } = renderElement({});
+      const submitButton = getByRole('button', { name: '추가' });
+      expect(submitButton).toBeInTheDocument();
+    });
 
-    expect(submitButton).toBeInTheDocument();
+    it('renders 저장 when expense is provided.', () => {
+      const { getByRole } = renderElement({
+        expense: {
+          uid: '',
+          date: new Date(),
+          memo: '',
+          categories: [],
+          amount: 0,
+        },
+      });
+      const submitButton = getByRole('button', { name: '저장' });
+      expect(submitButton).toBeInTheDocument();
+    });
   });
 });

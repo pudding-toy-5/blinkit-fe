fb310d38854de17a2507249565d427592a28e402
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-02 19:06:09 +0900

    test: add base field test of ExpenseForm

--- src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
+++ src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
@@ -9,6 +9,45 @@ describe('ExpenseForm', () => {
   }: ExpenseFormProps) =>
     render(<ExpenseForm submitButtonText={submitButtonText} />);
 
+  describe('date', () => {
+    it('renders label.', () => {
+      const { getByLabelText } = renderElement({});
+      const label = getByLabelText('날짜');
+      expect(label).toBeInTheDocument();
+    });
+  });
+
+  describe('memo', () => {
+    it('renders label.', () => {
+      const { getByLabelText } = renderElement({});
+      const label = getByLabelText('메모');
+      expect(label).toBeInTheDocument();
+    });
+  });
+
+  describe('category', () => {
+    it('renders label.', () => {
+      const { getByLabelText } = renderElement({});
+      const label = getByLabelText('카테고리');
+      expect(label).toBeInTheDocument();
+    });
+
+    it('renders CategorySettingButton.', () => {
+      const { getByLabelText } = renderElement({});
+      const buttonLabel = getByLabelText('카테고리 설정 버튼');
+      expect(buttonLabel).toBeInTheDocument();
+      expect(buttonLabel.tagName.toLowerCase()).toBe('button');
+    });
+  });
+
+  describe('amount', () => {
+    it('renders label.', () => {
+      const { getByLabelText } = renderElement({});
+      const label = getByLabelText('금액');
+      expect(label).toBeInTheDocument();
+    });
+  });
+
   it('renders provided submitButtonText.', () => {
     const text = 'submit button text example';
     const { getByRole } = renderElement({ submitButtonText: text });

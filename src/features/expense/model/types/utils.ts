b929063d111f8ea36a3f20b9c08712301d5242fc
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-03 05:34:08 +0900

    refact: add handling error on convertExpenseToServerExpense utils

--- src/features/expense/model/types/utils.ts
+++ src/features/expense/model/types/utils.ts
@@ -5,24 +5,28 @@ export const convertExpenseToServerExpense = (
 ): ServerExpense => {
   const { uid, date, memo, amount, categories } = expense;
 
-  return {
-    uid,
-    expended_at: date?.toISOString(),
-    memo,
-    amount: amount?.toString(),
-    categories,
-  } as ServerExpense;
+  const serverExpense: ServerExpense = {
+    uid: uid ?? '',
+    expended_at: date ? date.toISOString() : new Date().toISOString(),
+    memo: memo ?? '',
+    amount: amount ? amount.toString() : '0',
+    categories: categories ?? [],
+  };
+
+  return serverExpense;
 };
 
 export const convertServerExpenseToExpense = (
   serverExpense: ServerExpense
 ): Expense => {
   const { uid, expended_at, memo, amount, categories } = serverExpense;
+  const parsedAmount = parseInt(amount);
+
   return {
     uid,
     date: new Date(expended_at),
     memo,
-    amount: parseInt(amount),
+    amount: isNaN(parsedAmount) ? 0 : parsedAmount,
     categories,
   } as Expense;
 };

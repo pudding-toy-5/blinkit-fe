e1f67816b1e8c8b23075123c2fa46ae768e9a7c0
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-03 08:42:51 +0900

    feat: add useCategories to render categories

--- src/features/category/ui/routes/CategoriesRoute/CategoriesRoute.tsx
+++ src/features/category/ui/routes/CategoriesRoute/CategoriesRoute.tsx
@@ -1,6 +1,8 @@
+import { Link } from '@tanstack/react-router';
 import React from 'react';
 
 import { Button } from '@/components/ui/button';
+import { useCategories } from '@/features/category/api/useCategoryQuery';
 import CategoryTag from '@/features/category/ui/CategoryTag';
 import InputCategoryTags from '@/features/category/ui/InputCategoryTags';
 import Ellipsis from '@/shared/ui/icons/Ellipsis';
@@ -8,6 +10,8 @@ import Layout from '@/shared/ui/layout/Layout';
 import SubPageHeader from '@/shared/ui/SubPageHeader';
 
 const CategoriesRoute: React.FC = () => {
+  const { categories } = useCategories();
+
   const [values, setValues] = React.useState<string[]>([]);
 
   return (
@@ -23,15 +27,19 @@ const CategoriesRoute: React.FC = () => {
       <div className='flex flex-col px-5 pt-9 pb-6 h-screen'>
         <p className='text-[13px] font-semibold text-[#999]'>카테고리 선택</p>
         <ul className='flex flex-col gap-4 list-none overflow-y-scroll scroll mt-4'>
-          {values.map((value) => (
-            <li className='flex flex-row items-center' key={value}>
-              <CategoryTag tagName={value} size='medium' />
-              <Button
-                variant='ghost'
-                className='size-6 p-0 ml-auto'
-                // onClick={}
-              >
-                <Ellipsis size={24} color='#555' />
+          {categories.map((category) => (
+            <li className='flex flex-row items-center' key={category.uid}>
+              <CategoryTag tagName={category.name} size='medium' />
+              <Button variant='ghost' className='size-6 p-0 ml-auto' asChild>
+                <Link
+                  to='/categories/$uid'
+                  params={(prev: { uid?: string }) => ({
+                    ...prev,
+                    uid: category.uid,
+                  })}
+                >
+                  <Ellipsis size={24} color='#555' />
+                </Link>
               </Button>
             </li>
           ))}

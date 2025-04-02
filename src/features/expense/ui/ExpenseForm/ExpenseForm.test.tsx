193e485c3734a8e20aa630f2b8b27bb090ae529d
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-02 17:58:21 +0900

    test: update ExpenseForm with submitButtonText test

--- src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
+++ src/features/expense/ui/ExpenseForm/ExpenseForm.test.tsx
@@ -1,24 +1,19 @@
-import { describe, it } from 'vitest';
 import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
 
-import ExpenseForm from './ExpenseForm';
+import ExpenseForm, { ExpenseFormProps } from './ExpenseForm';
 
 describe('ExpenseForm', () => {
-  const renderExpenseForm = () => render(<ExpenseForm />);
+  const renderElement = ({
+    submitButtonText = 'submit button text',
+  }: ExpenseFormProps) =>
+    render(<ExpenseForm submitButtonText={submitButtonText} />);
 
-  it('renders label elements.', () => {
-    const { getByLabelText } = renderExpenseForm();
+  it('renders provided submitButtonText.', () => {
+    const text = 'submit button text example';
+    const { getByRole } = renderElement({ submitButtonText: text });
+    const submitButton = getByRole('button', { name: text });
 
-    expect(getByLabelText('날짜')).toBeInTheDocument();
-    expect(getByLabelText('카테고리')).toBeInTheDocument();
-    expect(getByLabelText('메모')).toBeInTheDocument();
-    expect(getByLabelText('금액')).toBeInTheDocument();
-  });
-
-  it(`renders '저장' submit button.`, () => {
-    const { getByRole } = renderExpenseForm();
-    const submitButton = getByRole('button', { name: '저장' });
-
-    expect(submitButton).not.toBeNull();
+    expect(submitButton).toBeInTheDocument();
   });
 });

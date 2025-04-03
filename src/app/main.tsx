3af31410c85e523cb79d6209389f1db713985682
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-03 23:39:11 +0900

    refact: use routes.custom in main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,41 +5,13 @@ import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // tanstack-router
-import {
-  createRoute,
-  createRouter,
-  Outlet,
-  RouterProvider,
-} from '@tanstack/react-router';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDom from 'react-dom/client';
 
-import PrivacyPage from '@/pages/about/agreements/PrivacyPage';
-import TermsPage from '@/pages/about/agreements/TermsPage';
 import Toaster from '@/shared/ui/Toaster';
 
-import { routeTree } from './routeTree.gen';
-
-const agreementsRoute = createRoute({
-  getParentRoute: () => routeTree,
-  path: '/about/agreements',
-  component: Outlet,
-});
-
-const termsRoute = createRoute({
-  getParentRoute: () => agreementsRoute,
-  path: 'terms.html',
-  component: TermsPage,
-});
-
-const privacyRoute = createRoute({
-  getParentRoute: () => agreementsRoute,
-  path: 'privacy.html',
-  component: PrivacyPage,
-});
-
-agreementsRoute.addChildren([termsRoute, privacyRoute]);
-routeTree.addChildren([agreementsRoute]);
+import { routeTree } from './routes.custom';
 
 const router = createRouter({ routeTree });
 const queryClient = new QueryClient();

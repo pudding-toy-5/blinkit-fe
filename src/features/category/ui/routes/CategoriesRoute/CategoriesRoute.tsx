34b123d7d54d92e15c6f6aee561bd021110af56c
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-03 13:24:54 +0900

    feat: add type-safety on render category ul in CategoriesRoute

--- src/features/category/ui/routes/CategoriesRoute/CategoriesRoute.tsx
+++ src/features/category/ui/routes/CategoriesRoute/CategoriesRoute.tsx
@@ -27,22 +27,23 @@ const CategoriesRoute: React.FC = () => {
       <div className='flex flex-col px-5 pt-9 pb-6 h-screen'>
         <p className='text-[13px] font-semibold text-[#999]'>카테고리 선택</p>
         <ul className='flex flex-col gap-4 list-none overflow-y-scroll scroll mt-4'>
-          {categories.map((category) => (
-            <li className='flex flex-row items-center' key={category.uid}>
-              <CategoryTag tagName={category.name} size='medium' />
-              <Button variant='ghost' className='size-6 p-0 ml-auto' asChild>
-                <Link
-                  to='/categories/$uid'
-                  params={(prev: { uid?: string }) => ({
-                    ...prev,
-                    uid: category.uid,
-                  })}
-                >
-                  <Ellipsis size={24} color='#555' />
-                </Link>
-              </Button>
-            </li>
-          ))}
+          {categories &&
+            categories.map((category) => (
+              <li className='flex flex-row items-center' key={category.uid}>
+                <CategoryTag tagName={category.name} size='medium' />
+                <Button variant='ghost' className='size-6 p-0 ml-auto' asChild>
+                  <Link
+                    to='/categories/$uid'
+                    params={(prev: { uid?: string }) => ({
+                      ...prev,
+                      uid: category.uid,
+                    })}
+                  >
+                    <Ellipsis size={24} color='#555' />
+                  </Link>
+                </Button>
+              </li>
+            ))}
         </ul>
         <Button className='h-13 rounded-full text-[15px] mt-auto'>완료</Button>
       </div>

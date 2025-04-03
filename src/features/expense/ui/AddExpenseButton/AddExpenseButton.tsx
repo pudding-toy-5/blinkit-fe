1b094cdf3019a52446f3e0538e4f23a53676c405
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-03 19:59:59 +0900

    remove: remove onClick in AddExpenseButton

--- src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
+++ src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
@@ -1,13 +1,8 @@
 import { Plus } from 'lucide-react';
 
-interface AddExpenseButtonProps {
-  onClick: () => void;
-}
-
-const AddExpenseButton: React.FC<AddExpenseButtonProps> = ({ onClick }) => {
+const AddExpenseButton: React.FC = () => {
   return (
     <div
-      onClick={onClick}
       aria-label='지출 추가'
       className='absolute right-[20px] bottom-[32px] w-14 h-14 p-4 rounded-[28px] bg-[#89f336] hover:ring'
       role='button'

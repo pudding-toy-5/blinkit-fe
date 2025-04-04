c94af50f627b0c5d6c5aa06861524e8ab0add705
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-04 17:54:42 +0900

    feat: add basic login route

---
+++ src/app/routes/login.tsx
@@ -0,0 +1,30 @@
+import { createFileRoute, Link } from '@tanstack/react-router';
+
+import SignInButton from '@/features/sign-in/ui/SignInButton';
+import Layout from '@/shared/ui/layout/Layout';
+
+export const Route = createFileRoute('/login')({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <Layout>
+      <div></div>
+      <div className='flex flex-col gap-3 w-full px-5 mt-auto'>
+        <SignInButton service='google' />
+        <SignInButton service='naver' />
+      </div>
+      <div className='flex flex-col items-center my-8'>
+        <p className='text-[13px] text-[#555]'>
+          회원가입 시{' '}
+          <Link to='/about/agreements/terms.html'>서비스 이용약관</Link>과
+        </p>
+        <p className='text-[13px] text-[#555]'>
+          <Link to='/about/agreements/privacy.html'>개인정보 수집 및 이용</Link>
+          에 동의하게 됩니다.
+        </p>
+      </div>
+    </Layout>
+  );
+}

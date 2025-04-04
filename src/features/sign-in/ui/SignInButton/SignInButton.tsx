ca876206a744a55a919694a904c95a52c2e45797
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-04 18:08:40 +0900

    style: add rounded-xl to SignInButton

--- src/features/sign-in/ui/SignInButton/SignInButton.tsx
+++ src/features/sign-in/ui/SignInButton/SignInButton.tsx
@@ -182,7 +182,7 @@ export default function SocialAuth({ service, onClick }: SocialAuthProps) {
             // eslint-disable-next-line @typescript-eslint/no-misused-promises
             onClick={handleLogin}
             className={cn(
-              'flex h-12 w-full items-center justify-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-md hover:bg-gray-50 transition-colors',
+              'flex h-12 w-full items-center justify-center gap-2 px-4 py-2 bg-white border border-gray-300 rounded-md hover:bg-gray-50 transition-colors rounded-xl',
               service === 'google' &&
                 'bg-white text-[#222] text-[15px] border-[#ccc] hover:bg-white/90',
               service === 'naver' &&

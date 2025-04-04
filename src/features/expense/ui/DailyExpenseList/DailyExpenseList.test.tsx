01e2b0a23f90c904484fa2b8bc8a98cd13ab1133
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-04 16:20:11 +0900

    test: add DailyExpenseList test onScroll

--- src/features/expense/ui/DailyExpenseList/DailyExpenseList.test.tsx
+++ src/features/expense/ui/DailyExpenseList/DailyExpenseList.test.tsx
@@ -1,21 +1,32 @@
 import { render } from '@testing-library/react';
-import { describe } from 'vitest';
+import { describe, vi } from 'vitest';
 
 import DailyExpenseList, { DailyExpenseListProps } from './DailyExpenseList';
 
 describe('DailyExpenseList', () => {
-  const renderDailyExpenseList = ({ dailyExpenses }: DailyExpenseListProps) =>
-    render(<DailyExpenseList dailyExpenses={dailyExpenses} />);
+  const renderDailyExpenseList = ({
+    dailyExpenses,
+    onScroll = vi.fn(),
+  }: DailyExpenseListProps) =>
+    render(
+      <DailyExpenseList dailyExpenses={dailyExpenses} onScroll={onScroll} />
+    );
 
   it('when length of dailyExpenses is 0, renders help text.', () => {
-    const { getByText } = renderDailyExpenseList({ dailyExpenses: [] });
+    const { getByText } = renderDailyExpenseList({
+      dailyExpenses: [],
+      onScroll: vi.fn(),
+    });
     const text = getByText('지출 내역을 추가해주세요.');
 
     expect(text).toBeInTheDocument();
   });
 
   it('renders daily-expenses-list label', () => {
-    const { getByLabelText } = renderDailyExpenseList({ dailyExpenses: [] });
+    const { getByLabelText } = renderDailyExpenseList({
+      dailyExpenses: [],
+      onScroll: vi.fn(),
+    });
     const label = getByLabelText('daily-expense-list');
 
     expect(label).toBeInTheDocument();

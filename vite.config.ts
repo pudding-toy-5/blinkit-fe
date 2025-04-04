1426655d045ae56d88ffbebf2ff94da0e2cd1b1e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-04 15:08:04 +0900

    lint: lint config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({

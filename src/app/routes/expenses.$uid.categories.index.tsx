fe7a5f0fabce88c505f9d0887d4cdee2cb0df3c4
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-05 20:51:13 +0900

    refact: update expense.uid.categories

--- src/app/routes/expenses.$uid.categories.index.tsx
+++ src/app/routes/expenses.$uid.categories.index.tsx
@@ -1,39 +1,100 @@
-import { createFileRoute, useLoaderData } from '@tanstack/react-router';
+import {
+  createFileRoute,
+  useLoaderData,
+  useNavigate,
+} from '@tanstack/react-router';
 import { Link } from '@tanstack/react-router';
 import React from 'react';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
 import {
   useAddCategory,
   useCategories,
 } from '@/features/category/api/useCategoryQuery';
+import { Category } from '@/features/category/model/types/Category';
 import CategoryTag from '@/features/category/ui/CategoryTag';
 import InputCategoryTags from '@/features/category/ui/InputCategoryTags';
+import {
+  useExpenseByUid,
+  useUpdateExpense,
+} from '@/features/expense/api/useExpenseQuery';
 import Ellipsis from '@/shared/ui/icons/Ellipsis';
 import Layout from '@/shared/ui/layout/Layout';
 import SubPageHeader from '@/shared/ui/SubPageHeader';
 
 export const Route = createFileRoute('/expenses/$uid/categories/')({
-  loader: ({ params: { uid } }) => {
-    return uid;
-  },
   component: ExpenseUidCategoriesRoute,
 });
 
 export function ExpenseUidCategoriesRoute() {
-  const uid = Route.useLoaderData();
-  const addCategory = useAddCategory();
-  const { categories } = useCategories();
+  const navigate = useNavigate();
+  const { uid }: { uid: string } = Route.useParams();
 
-  const [inputCategories, setInputCategories] = React.useState<string[]>([]);
+  const addCategory = useAddCategory();
+  const updateExpense = useUpdateExpense();
+  const { data: expense } = useExpenseByUid(uid);
+  const { categories, isLoading, isError, error } = useCategories();
+
+  const [values, setValues] = React.useState<string[]>(
+    expense ? expense.categories.map((category) => category.name) : []
+  );
+
+  if (isLoading) {
+    return <>Loading</>;
+  }
+
+  if (isError && error) {
+    throw error;
+  }
+
+  const handleSetValues = (newValues: string[]) => {
+    if (categories === undefined) {
+      setValues(newValues);
+      return;
+    }
+
+    const notAddedValues = newValues.filter(
+      (value) => !categories.some((c) => c.name === value)
+    );
+    console.log(notAddedValues);
+
+    notAddedValues.forEach((notAddedValue) => {
+      addCategory.mutate({ name: notAddedValue });
+    });
+
+    setValues(newValues);
+  };
+
+  const onClickCategory = (clickedCategory: Category) => {
+    if (values.length >= 3) {
+      toast.error('카테고리는 최대 3개까지 선택할 수 있어요.');
+      return;
+    }
+
+    if (values.find((value) => clickedCategory.name === value)) {
+      toast.error('추가하려는 카테고리는 이미 선택되었어요.');
+      return;
+    }
+
+    setValues([...values, clickedCategory.name]);
+  };
+
+  const onClickSubmit = () => {
+    const selectedCategories =
+      categories?.filter((category) => values.includes(category.name)) ?? [];
+
+    updateExpense.mutate({ ...expense, categories: selectedCategories });
+    void navigate({ to: '/expenses/' + uid });
+  };
 
   return (
     <Layout guarded>
       <SubPageHeader title='카테고리 설정' close />
       <div className='mt-6 px-5'>
         <InputCategoryTags
-          value={inputCategories}
-          onChange={setInputCategories}
+          value={values}
+          onChange={handleSetValues}
           placeholder='카테고리명을 입력해주세요. (예: 카페)'
         />
       </div>
@@ -49,7 +110,13 @@ export function ExpenseUidCategoriesRoute() {
               const category_uid = category.uid;
               return (
                 <li className='flex flex-row items-center' key={category.uid}>
-                  <CategoryTag tagName={category.name} size='medium' />
+                  <CategoryTag
+                    tagName={category.name}
+                    size='medium'
+                    onClick={() => {
+                      onClickCategory(category);
+                    }}
+                  />
                   <Button
                     variant='ghost'
                     className='size-6 p-0 ml-auto'
@@ -67,7 +134,12 @@ export function ExpenseUidCategoriesRoute() {
             })}
           </ul>
         )}
-        <Button className='h-13 rounded-full text-[15px] mt-auto'>완료</Button>
+        <Button
+          className='h-13 rounded-full text-[15px] mt-auto'
+          onClick={onClickSubmit}
+        >
+          완료
+        </Button>
       </div>
     </Layout>
   );

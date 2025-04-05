89669539f70cb1c675aa0c6e62ece3cd66cb3a47
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-05 13:32:49 +0900

    feat: add useUpdateMe in useAuth

--- src/features/auth/api/useAuth.ts
+++ src/features/auth/api/useAuth.ts
@@ -2,7 +2,10 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
 import { ServerUser, User } from '@/features/auth/model/User';
-import { convertServerUserToUser } from '@/features/auth/model/utils';
+import {
+  convertServerUserToUser,
+  convertUserToServerUser,
+} from '@/features/auth/model/utils';
 import { apiUrl } from '@/features/common/consts';
 import userAxios from '@/shared/api/userAxios';
 
@@ -25,10 +28,34 @@ export const useGetMe = () => {
   });
 };
 
-// export const useUpdateMe = () => {
-//   const queryClient = useQueryClient();
+export const useUpdateMe = () => {
+  const queryClient = useQueryClient();
 
-//   return useMutation({
-//     mutationFn: async,
-//   });
-// };
+  return useMutation({
+    mutationFn: async (user: Partial<User>): Promise<User> => {
+      try {
+        if (user.uid !== undefined) {
+          throw new Error('useUpdateMe - uid undefined');
+        }
+        const res = await userAxios.patch(
+          `${apiUrl}/account/users/me`,
+          convertUserToServerUser(user)
+        );
+        return convertServerUserToUser(res.data as ServerUser);
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          throw new Error('useUpdateMe failed: ' + error.message);
+        }
+        throw error;
+      }
+    },
+    onSuccess: (updatedUser) => {
+      queryClient.setQueryData(['me'], () => {
+        return updatedUser;
+      });
+    },
+    onError: (error) => {
+      console.error('failed on useUpdateExpense: ', error);
+    },
+  });
+};

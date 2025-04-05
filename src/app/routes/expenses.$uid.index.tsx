122076abd94047dfe0209af459705bddbe74eb9c
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-05 20:31:55 +0900

    rename: rename expense.$uid to expenses.uid.index
    add loader with uid
    add uid to ExpenseForm

--- src/app/routes/expenses.$uid.index.tsx
+++ src/app/routes/expenses.$uid.index.tsx
@@ -5,9 +5,9 @@ import ExpenseForm from '@/features/expense/ui/ExpenseForm';
 import Layout from '@/shared/ui/layout/Layout';
 import SubPageHeader from '@/shared/ui/SubPageHeader';
 
-export const Route = createFileRoute('/expenses/$uid')({
+export const Route = createFileRoute('/expenses/$uid/')({
+  loader: ({ params: { uid } }) => uid,
   component: RouteComponent,
-  notFoundComponent: () => <p>expense with id not found</p>,
 });
 
 export function RouteComponent() {
@@ -36,7 +36,7 @@ export function RouteComponent() {
   return (
     <Layout guarded>
       <SubPageHeader title='지출 내역 수정' back />
-      <ExpenseForm expense={expense} />
+      <ExpenseForm uid={uid} expense={expense} />
     </Layout>
   );
 }

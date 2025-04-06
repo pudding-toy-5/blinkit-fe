a39fc9a481bd05dd50a8a389f91f4a44940b6efe
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-06 20:42:29 +0900

    refact: add Form to settings.account

--- src/app/routes/settings.account.tsx
+++ src/app/routes/settings.account.tsx
@@ -1,6 +1,9 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
+import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { useMe, useUpdateMe } from '@/features/auth/api/useAuth';
@@ -12,26 +15,51 @@ export const Route = createFileRoute('/settings/account')({
   component: RouteComponent,
 });
 
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
+import SubmitButton from '@/shared/ui/SubmitButton';
+
+const formSchema = z.object({
+  nickname: z
+    .string()
+    .min(1, {
+      message: '닉네임은 최소 1글자 이상이어야 해요.',
+    })
+    .max(20, {
+      message: '닉네임은 최대 20글자까지 입력할 수 있어요.',
+    }),
+  email: z.string(),
+});
+
 function RouteComponent() {
   const navigate = useNavigate();
   const updateMe = useUpdateMe();
   const { data, isError, error } = useMe();
 
-  const [nickname, setNickname] = React.useState<string>('');
-  const [email, setEmail] = React.useState<string>('');
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: { nickname: '', email: '' },
+  });
 
   if (isError) {
     throw error;
   }
 
   React.useEffect(() => {
-    if (data?.nickname !== undefined && data.email !== undefined) {
-      setNickname(data.nickname ?? '');
-      setEmail(data.email);
+    if (data) {
+      if (data.nickname !== undefined) {
+        form.setValue('nickname', data.nickname);
+      }
+
+      if (data.email !== undefined) {
+        form.setValue('email', data.email);
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
-  const onSubmit = () => {
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const nickname = values.nickname;
+
     if (nickname.length === 0) {
       toast.error('닉네임은 최소 1글자 이상이어야 해요.');
       return;
@@ -49,16 +77,13 @@ function RouteComponent() {
       // data.isSuperuser !== undefined
     ) {
       updateMe.mutate(
-        { uid: data.uid, email: data.email, nickname },
+        { ...data, nickname },
         {
           onSuccess: () => {
-            toast.success('회원정보를 성공적으로 업데이트했습니다.');
             void navigate({ to: '/expenses' });
           },
-          onError: (error) => {
-            toast.error(
-              '회원정보 업데이트 중 오류가 발생했습니다. ' + error.message
-            );
+          onError: () => {
+            toast.error('닉네임 설정에 실패했어요.');
           },
         }
       );
@@ -68,20 +93,51 @@ function RouteComponent() {
   return (
     <Layout guarded>
       <SubPageHeader title='회원정보' back />
-      <LabeledTextInput
-        id='nickname'
-        label='닉네임'
-        value={nickname}
-        onChange={setNickname}
-        maxLength={20}
-      />
-      <LabeledTextInput
-        id='email'
-        label='이메일'
-        value={email}
-        state='disabled'
-      />
-      <Button onClick={onSubmit}>저장</Button>
+      <Form {...form}>
+        <form
+          className='flex flex-col flex-1'
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name='nickname'
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <LabeledTextInput
+                    id='nickname'
+                    label='닉네임'
+                    value={field.value}
+                    onChange={field.onChange}
+                    maxLength={20}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <LabeledTextInput
+                    id='email'
+                    label='이메일'
+                    value={field.value}
+                    onChange={field.onChange}
+                    state='disabled'
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className='mt-auto mb-8 px-5'>
+            <SubmitButton type='submit' text='저장' className='w-full' />
+          </div>
+        </form>
+      </Form>
     </Layout>
   );
 }

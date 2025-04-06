c20fedaf93da8074d9d148c3876bf5e6f6272e59
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-06 17:30:50 +0900

    feat: add validation on shared/ui/layout

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,17 +1,36 @@
 import { useNavigate } from '@tanstack/react-router';
+import { toast } from 'sonner';
 
 import { TOKEN_KEY } from '@/constants';
+import { useMe } from '@/features/auth/api/useAuth';
 
 const Layout: React.FC<{
   children?: React.ReactNode;
   guarded?: boolean;
 }> = ({ children, guarded }) => {
   const navigate = useNavigate();
-  if (guarded) {
-    const token = localStorage.getItem(TOKEN_KEY);
-    if (!token) {
-      void navigate({ to: '/login' });
-    }
+  const { isError } = useMe();
+
+  if (!guarded) {
+    return (
+      <div className='max-w-sm mx-auto h-screen bg-white relative flex flex-col overflow-hidden'>
+        {children}
+      </div>
+    );
+  }
+
+  const token = localStorage.getItem(TOKEN_KEY);
+
+  if (!token) {
+    toast.error('소셜 로그인에서 문제가 발생했어요.');
+    void navigate({ to: '/login' });
+    return;
+  }
+
+  if (isError) {
+    toast.error('소셜 로그인에서 문제가 발생했어요.');
+    void navigate({ to: '/login' });
+    return;
   }
 
   return (

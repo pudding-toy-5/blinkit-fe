19e6103106fba2ed17f28002936d83b2b9e401fc
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-07 20:05:23 +0900

    feat: add check nickname in index route

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,19 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router';
 
+import { useMe } from '@/features/auth/api/useAuth';
+
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const navigate = useNavigate();
+  const { data: user } = useMe();
+
+  if (user?.nickname === '') {
+    void navigate({ to: '/settings/account' });
+    return;
+  }
 
   void navigate({ to: '/expenses' });
   return;

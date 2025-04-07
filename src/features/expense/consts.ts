8dafbd045511e84204f8f6f15f08965f3caa60f9
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-07 19:05:09 +0900

    fix: fix EXPENSE_AMOUNT_MAX

--- src/features/expense/consts.ts
+++ src/features/expense/consts.ts
@@ -1,6 +1,6 @@
 export const EXPENSE_MEMO_MAX_LEN = 120;
 
-export const EXPENSE_AMOUNT_MAX = 100000000000;
+export const EXPENSE_AMOUNT_MAX = 100000000000 - 1;
 
 export const queryKeys = {
   expenses: ['expenses'],

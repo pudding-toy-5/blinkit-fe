c827e01ee77743e034c70ca145a77b59f3f57c90
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-07 16:58:42 +0900

    feat: add onClose and Drawer to SubPageHeader

--- src/shared/ui/SubPageHeader/SubPageHeader.tsx
+++ src/shared/ui/SubPageHeader/SubPageHeader.tsx
@@ -1,20 +1,32 @@
 import { useRouter } from '@tanstack/react-router';
+import { X } from 'lucide-react';
 
-import { buttonVariants } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 import ArrowLeft from '@/shared/ui/icons/ArrowLeft';
-import X from '@/shared/ui/icons/X';
 import { cn } from '@/shared/ui/styles/utils';
 
 export interface SubPageHeaderProps {
   title: string;
   back?: boolean;
   close?: boolean;
+  onClose?: () => void;
 }
 
 const SubPageHeader: React.FC<SubPageHeaderProps> = ({
   title,
   back,
   close,
+  onClose,
 }) => {
   const router = useRouter();
 
@@ -23,7 +35,12 @@ const SubPageHeader: React.FC<SubPageHeaderProps> = ({
   };
 
   const handleClickClose = () => {
-    router.history.back();
+    if (!onClose) {
+      router.history.back();
+      return;
+    }
+
+    onClose();
   };
 
   return (
@@ -58,6 +75,39 @@ const SubPageHeader: React.FC<SubPageHeaderProps> = ({
         >
           <X size={24} />
         </div>
+      )}
+      {onClose && (
+        <Drawer>
+          <DrawerTrigger
+            className={cn(
+              buttonVariants({ variant: 'ghost' }),
+              'text-[15px] text-[#222] bg-[#efefef] hover:bg-[#efefef]/80',
+              'absolute right-5 h-auto p-0'
+            )}
+          >
+            삭제
+          </DrawerTrigger>
+          <DrawerContent className='w-full max-w-sm mx-auto py-8 px-5 rounded-t-[20px]'>
+            <DrawerHeader className='p-0'>
+              <DrawerTitle className='text-[19px] text-[#222] font-semibold'>
+                내역을 삭제할까요?
+              </DrawerTitle>
+              <DrawerDescription className='text-15px text-[#555]'>
+                선택한 내역이 지출 내역에서 삭제돼요.
+              </DrawerDescription>
+            </DrawerHeader>
+            <DrawerFooter className='p-0 mt-9'>
+              <Button className='h-13 rounded-full' onClick={handleClickClose}>
+                삭제
+              </Button>
+              <DrawerClose>
+                <Button variant='ghost' className='h-13 w-full rounded-full'>
+                  취소
+                </Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
       )}
     </header>
   );

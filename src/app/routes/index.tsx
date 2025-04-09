3b02eb9e05d4bb3b0b0a6f70822d0eba29ed5b28
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-09 18:06:25 +0900

    feat: add toast on index route error

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
+import { toast } from 'sonner';
 
 import { ServerUser } from '@/features/auth/model/User';
 import { convertServerUserToUser } from '@/features/auth/model/utils';
@@ -12,15 +13,18 @@ export const Route = createFileRoute('/')({
       const user = convertServerUserToUser(res.data);
 
       if (!user.nickname) {
+        toast.error('닉네임을 설정한 후 서비스를 사용할 수 있어요.');
         return redirect({ to: '/settings/account' });
       }
 
       if (user.nickname === '') {
+        toast.error('닉네임을 설정한 후 서비스를 사용할 수 있어요.');
         return redirect({ to: '/settings/account' });
       }
 
       return redirect({ to: '/expenses' });
     } catch {
+      toast.error('소셜 로그인에서 문제가 발생했어요.');
       return redirect({ to: '/login' });
     }
   },

8c53dad3a3666a6fc7bb7fe284071f9ed80e0911
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-09 21:12:59 +0900

    feat: add Layout to GuestLayout

--- src/shared/ui/layout/GuestLayout.tsx
+++ src/shared/ui/layout/GuestLayout.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
+import Layout from './Layout';
+
 const GuestLayout: React.FC<{
   children?: React.ReactNode;
 }> = ({ children }) => {
-  return (
-    <div className='w-full min-w-[375px] max-w-[430px] mx-auto h-screen bg-white relative flex flex-col overflow-hidden safe-area-wrapper'>
-      {children}
-    </div>
-  );
+  return <Layout>{children}</Layout>;
 };
 
 export default GuestLayout;

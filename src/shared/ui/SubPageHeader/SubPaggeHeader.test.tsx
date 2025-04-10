20f0965fd8d7c113095e9dd7991d55efc8dbf506
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-10 15:00:26 +0900

    REMOVE unused import

--- src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
+++ src/shared/ui/SubPageHeader/SubPaggeHeader.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 
 // mock react-router useRouter().history.back()

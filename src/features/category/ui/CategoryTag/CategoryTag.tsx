7511e7ac85746cefb03a0d1f5dea5a5868fc0e7c
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-10 20:59:12 +0900

    style: remove h-6 in CategoryTag

--- src/features/category/ui/CategoryTag/CategoryTag.tsx
+++ src/features/category/ui/CategoryTag/CategoryTag.tsx
@@ -20,7 +20,7 @@ const CategoryTag: React.FC<CategoryTagProps> = ({
     <Badge
       className={cn(
         'flex flex-row items-center bg-[#EAF6EC] text-[#28A745] rounded-full text-[13px]',
-        'px-2 py-1 h-6',
+        'px-2 py-1',
         size === 'small' && 'px-2 py-1',
         size === 'medium' && 'px-3 py-2',
         size === 'large' && 'px-4 py-3'

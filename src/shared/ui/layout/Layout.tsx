1fa9cb5d52c2f77a3aa8dfa24fe387a20dff012e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-11 21:39:15 +0900

    style: add height 100dvh, remove safe-area-wrapper in Layout

--- src/shared/ui/layout/Layout.tsx
+++ src/shared/ui/layout/Layout.tsx
@@ -1,6 +1,6 @@
 const Layout: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
   return (
-    <div className='w-full min-w-[375px] max-w-[430px] mx-auto h-screen bg-white relative flex flex-col overflow-hidden safe-area-wrapper'>
+    <div className='w-full min-w-[375px] max-w-[430px] mx-auto h-dvh bg-white relative flex flex-col overflow-hidden'>
       {children}
     </div>
   );

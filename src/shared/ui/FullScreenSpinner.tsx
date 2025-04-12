756a9bbdcf797ee2a76b044caf6a2390cb58cab0
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-12 13:25:01 +0900

    style: change min-width to 360px

--- src/shared/ui/FullScreenSpinner.tsx
+++ src/shared/ui/FullScreenSpinner.tsx
@@ -1,5 +1,5 @@
 const FullScreenSpinner: React.FC = () => (
-  <div className='w-full min-w-[375px] max-w-[430px] mx-auto h-screen bg-white relative flex flex-col overflow-hidden safe-area-wrapper'>
+  <div className='w-full min-w-[360px] max-w-[430px] mx-auto h-screen bg-white relative flex flex-col overflow-hidden safe-area-wrapper'>
     <div className='flex flex-1 justify-center items-center'>
       <div className='animate-spin rounded-full h-8 w-8 border-b-2 border-[#222]' />
     </div>

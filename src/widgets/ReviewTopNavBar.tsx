4e14961b6cf7cc290825a31ab91e202f7e062a90
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-14 15:16:57 +0900

    rename: isClicked to isSelected in ReviewTopBar

--- src/widgets/ReviewTopNavBar.tsx
+++ src/widgets/ReviewTopNavBar.tsx
@@ -2,18 +2,18 @@ import { cn } from '@/shared/ui/styles/utils';
 
 interface NavItemProps {
   text: string;
-  isClicked: boolean;
+  isSelected: boolean;
   onClick: () => void;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ text, isClicked, onClick }) => {
+const NavItem: React.FC<NavItemProps> = ({ text, isSelected, onClick }) => {
   return (
     <li
       className={cn(
         'flex flex-1 items-center justify-center pb-[10px]',
         'text-[17px] text-[#999999] font-semibold',
         'border-b-[2px] border-transparent',
-        isClicked && 'text-[#222222] border-[#222222]'
+        isSelected && 'text-[#222222] border-[#222222]'
       )}
       onClick={onClick}
     >
@@ -33,14 +33,14 @@ const ReviewTopNavBar: React.FC<NavBarProps> = ({ isRewind, setIsRewind }) => {
       <ul className='flex flex-row items-center justify-center gap-5'>
         <NavItem
           text='리뷰'
-          isClicked={!isRewind}
+          isSelected={!isRewind}
           onClick={() => {
             setIsRewind(false);
           }}
         />
         <NavItem
           text='회고'
-          isClicked={isRewind}
+          isSelected={isRewind}
           onClick={() => {
             setIsRewind(true);
           }}

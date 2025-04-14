ec26110e24cd373f21e44d50af77ef5882796177
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-14 14:35:55 +0900

    feat: add Logo and TopNavigation markup

--- src/app/routes/expenses.review.index.tsx
+++ src/app/routes/expenses.review.index.tsx
@@ -1,9 +1,52 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import React from 'react';
+
+import Logo from '@/shared/ui/icons/Logo';
+import UserLayout from '@/shared/ui/layout/UserLayout';
+import { cn } from '@/shared/ui/styles/utils';
 
 export const Route = createFileRoute('/expenses/review/')({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/expenses/review/"!</div>
+  const [isRewind, setIsRewind] = React.useState<boolean>(false);
+
+  return (
+    <UserLayout>
+      <header className='px-5 py-4'>
+        <Logo />
+      </header>
+      <nav className='px-5 pt-4'>
+        <ul className='flex flex-row items-center justify-center gap-5'>
+          <li
+            className={cn(
+              'flex items-center justify-center w-[50%]',
+              'text-[17px] text-[#999999] font-semibold',
+              'border-b-[2px] border-transparent',
+              !isRewind && 'text-[#222222] border-[#222222]'
+            )}
+            onClick={() => {
+              setIsRewind(false);
+            }}
+          >
+            리뷰
+          </li>
+          <li
+            className={cn(
+              'flex items-center justify-center w-[50%]',
+              'text-[17px] text-[#999999] font-semibold',
+              'border-b-[2px] border-transparent',
+              isRewind && 'text-[#222222] border-[#222222]'
+            )}
+            onClick={() => {
+              setIsRewind(true);
+            }}
+          >
+            회고
+          </li>
+        </ul>
+      </nav>
+    </UserLayout>
+  );
 }

9a0f1e9f1439ba9e82267b4a3f67979c9097e8d1
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 02:38:02 +0900

    feat: add consumptionTexts to RetrospectiveDetailPopover

--- src/features/retrospective/ui/RetrospectiveDetailPopover.tsx
+++ src/features/retrospective/ui/RetrospectiveDetailPopover.tsx
@@ -1,6 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-import { ConsumptionKind } from '@/features/expense/model/types/ConsumptionKind';
+import {
+  consumptionConsciousTexts,
+  consumptionEmotionalTexts,
+  consumptionEssentialTexts,
+} from '@/features/expense/consts';
+import {
+  ConsumptionKind,
+  ConsumptionTexts,
+} from '@/features/expense/model/types/ConsumptionKind';
 import UserLayout from '@/shared/ui/layout/UserLayout';
 import SubPageHeader from '@/shared/ui/SubPageHeader';
 
@@ -15,20 +23,59 @@ const RetrospectiveDetailPopover: React.FC<Props> = ({
   consumptionKind,
   onClose,
 }) => {
-  const { data } = useExpenses({
-    period: { year: 2024, month: 4 },
+  const { data: expenses } = useExpenses({
+    period: { year: 2025, month: 4 },
     consumptionKind,
   });
 
+  const [consumptionTexts, setConsumptionTexts] =
+    useState<ConsumptionTexts | null>(null);
+
+  const [totalAmount, setTotalAmount] = useState<number | null>(null);
+
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    if (!expenses) {
+      return;
+    }
+
+    setTotalAmount(
+      expenses.reduce((total, expense) => total + expense.amount, 0)
+    );
+  }, []);
+
+  useEffect(() => {
+    if (consumptionKind === ConsumptionKind.conscious) {
+      setConsumptionTexts(consumptionConsciousTexts);
+    }
+
+    if (consumptionKind === ConsumptionKind.emotional) {
+      setConsumptionTexts(consumptionEmotionalTexts);
+    }
+
+    if (consumptionKind === ConsumptionKind.essential) {
+      setConsumptionTexts(consumptionEssentialTexts);
+    }
+  }, [consumptionKind]);
+
+  if (!expenses) {
+    return <div>asdf</div>;
+  }
 
   return (
     <div className='fixed z-10 top-0 left-0 w-full h-dvh flex flex-col overflow-hidden'>
       <UserLayout>
         <SubPageHeader onClickBack={onClose} />
-        <div className='flex-1 bg-gray-100'></div>
+        <div className='flex flex-col px-5'>
+          <h1 className='text-[19px] text-[#222] font-semibold'>
+            {consumptionTexts?.title}
+          </h1>
+          <span className='text-[13px] text-[#555] mt-1'>
+            {consumptionTexts?.description}
+          </span>
+          <span className='text-[22px] text-[#222] font-semibold mt-3'>
+            {totalAmount}원
+          </span>
+        </div>
       </UserLayout>
     </div>
   );

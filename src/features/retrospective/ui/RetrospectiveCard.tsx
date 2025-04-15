f68e4ce379fc468d012a34f83e30d94686c94dab
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 02:15:46 +0900

    style: add text-size, text-color to RetrospectiveCard desc

--- src/features/retrospective/ui/RetrospectiveCard.tsx
+++ src/features/retrospective/ui/RetrospectiveCard.tsx
@@ -38,7 +38,7 @@ const RetrospectiveCard: React.FC<RetrospectiveCardProps> = ({
           {totalCount}건
         </span>
       </div>
-      <span className='mt-1'>{description}</span>
+      <span className='text-[13px] text-[#555] mt-1'>{description}</span>
       <span className='text-[22px] text-[#222] font-semibold mt-3'>
         {totalAmount.toLocaleString()}원
       </span>

c9c9bee297e86914f70b15d35ed9395b62abace0
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 03:58:15 +0900

    feat: add totalExpenseLength to props, add placeholder

--- src/features/expense/ui/UnReviewedExpenseList/UnReviewedExpenseList.tsx
+++ src/features/expense/ui/UnReviewedExpenseList/UnReviewedExpenseList.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
 
+import { cn } from '@/shared/ui/styles/utils';
+
 import { useUpdateExpense } from '../../api/useExpenseQuery';
 import { ConsumptionKind } from '../../model/types/ConsumptionKind';
 import { Expense } from '../../model/types/Expense';
@@ -9,10 +11,15 @@ import UnReviewedExpenseListItem from './UnReviewedExpenseListItem';
 
 interface Props {
   expenses: Expense[];
+  totalExpenseLength: number;
 }
 
-const UnReviewedExpenseList: React.FC<Props> = ({ expenses }) => {
+const UnReviewedExpenseList: React.FC<Props> = ({
+  expenses,
+  totalExpenseLength,
+}) => {
   const updateExpense = useUpdateExpense();
+
   const [selectedUid, setSelectedUid] = useState<string | null>(null);
   const [selectedExpense, setSelectedExpense] = useState<Expense | null>(null);
 
@@ -60,10 +67,24 @@ const UnReviewedExpenseList: React.FC<Props> = ({ expenses }) => {
         setConsumptionKind={setConsumptionKind}
       />
       <div className='flex flex-col flex-1 bg-[#f5f3f0] px-5 pt-8'>
-        {expenses.length === 0 ? (
-          <span className='flex items-center justify-center m-auto text-[15px] text-[#555]'>
-            지출 내역을 추가하면 리뷰할 수 있어요.
-          </span>
+        {totalExpenseLength === 0 ? (
+          <div className='flex-1 flex flex-col items-center justify-center text-center'>
+            <span className='text-[15px] text-[#555] leading-[150%]'>
+              리뷰할 지출 내역이 없어요.
+              <br />
+              회고 탭으로 이동해 결과를 확인해주세요.
+            </span>
+            <button
+              className={cn(
+                'bg-white',
+                'border-[1px] border-[#ccc] rounded-full',
+                'px-3 py-2 mt-4',
+                'text-[13px] text-[#555]'
+              )}
+            >
+              회고 탭으로 이동하기
+            </button>
+          </div>
         ) : (
           <>
             <div className='pb-4'>
@@ -74,27 +95,26 @@ const UnReviewedExpenseList: React.FC<Props> = ({ expenses }) => {
                 지출 내역을 왼쪽으로 밀어서 소비를 분류해보세요.
               </span>
             </div>
-            <ul className='flex flex-col gap-2 pb-2'>
-              {expenses.map((expense) => (
-                <UnReviewedExpenseListItem
-                  key={expense.uid}
-                  expense={expense}
-                  buttonWidth={buttonWidth}
-                  onSelectReview={() => {
-                    setSelectedUid(expense.uid);
-                  }}
-                />
-              ))}
-            </ul>
+            {expenses.length === 0 ? (
+              <span className='flex items-center justify-center m-auto text-[15px] text-[#555]'>
+                지출 내역을 추가하면 리뷰할 수 있어요.
+              </span>
+            ) : (
+              <ul className='flex flex-col gap-2 pb-2'>
+                {expenses.map((expense) => (
+                  <UnReviewedExpenseListItem
+                    key={expense.uid}
+                    expense={expense}
+                    buttonWidth={buttonWidth}
+                    onSelectReview={() => {
+                      setSelectedUid(expense.uid);
+                    }}
+                  />
+                ))}
+              </ul>
+            )}
           </>
         )}
-        {/* 보이지 않는 측정용 버튼 */}
-        <button
-          ref={measureRef}
-          className='absolute -z-10 invisible whitespace-nowrap px-6 font-semibold text-[14px]'
-        >
-          리뷰하기
-        </button>
       </div>
     </>
   );

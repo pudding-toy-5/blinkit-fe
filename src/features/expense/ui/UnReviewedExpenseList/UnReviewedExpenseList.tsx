a8b5b302302782e3e9500f58c4881c6605aca72a
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-15 16:08:12 +0900

    feat: add Drawer to UnReviewedExpenseList

--- src/features/expense/ui/UnReviewedExpenseList/UnReviewedExpenseList.tsx
+++ src/features/expense/ui/UnReviewedExpenseList/UnReviewedExpenseList.tsx
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect, useRef, useState } from 'react';
 
 import { Expense } from '../../model/types/Expense';
+import ClassifyExpenseDrawer from './ClassifyExpenseDrawer';
 import UnReviewedExpenseListItem from './UnReviewedExpenseListItem';
 
 interface Props {
@@ -22,35 +23,42 @@ const UnReviewedExpenseList: React.FC<Props> = ({ expenses }) => {
   };
 
   return (
-    <div className='flex flex-col flex-1 bg-[#f5f3f0] px-5 pt-8'>
-      <div className='pb-4'>
-        <h1 className='text-[19px] text-[#222] font-semibold'>
-          리뷰하지 않은 지출 내역
-        </h1>
-        <span className='pt-1 text-[13px] text-[#555]'>
-          지출 내역을 왼쪽으로 밀어서 소비를 분류해보세요.
-        </span>
+    <>
+      <ClassifyExpenseDrawer
+        isOpen={selectedUid !== null}
+        onReviewExpense={onReviewExpense}
+        onOpenChange={onDrawerOpenChange}
+      />
+      <div className='flex flex-col flex-1 bg-[#f5f3f0] px-5 pt-8'>
+        <div className='pb-4'>
+          <h1 className='text-[19px] text-[#222] font-semibold'>
+            리뷰하지 않은 지출 내역
+          </h1>
+          <span className='pt-1 text-[13px] text-[#555]'>
+            지출 내역을 왼쪽으로 밀어서 소비를 분류해보세요.
+          </span>
+        </div>
+        <ul className='flex flex-col gap-2'>
+          {expenses.map((expense) => (
+            <UnReviewedExpenseListItem
+              key={expense.uid}
+              expense={expense}
+              buttonWidth={buttonWidth}
+              onClickReview={() => {
+                setSelectedUid(expense.uid);
+              }}
+            />
+          ))}
+        </ul>
+        {/* 보이지 않는 측정용 버튼 */}
+        <button
+          ref={measureRef}
+          className='absolute -z-10 invisible whitespace-nowrap px-6 font-semibold text-[14px]'
+        >
+          리뷰하기
+        </button>
       </div>
-      <ul className='flex flex-col gap-2'>
-        {expenses.map((expense) => (
-          <UnReviewedExpenseListItem
-            key={expense.uid}
-            expense={expense}
-            buttonWidth={buttonWidth}
-            onClick={() => {
-              onClick(expense.uid);
-            }}
-          />
-        ))}
-      </ul>
-      {/* 보이지 않는 측정용 버튼 */}
-      <button
-        ref={measureRef}
-        className='absolute -z-10 invisible whitespace-nowrap px-6 font-semibold text-[14px]'
-      >
-        리뷰하기
-      </button>
-    </div>
+    </>
   );
 };
 

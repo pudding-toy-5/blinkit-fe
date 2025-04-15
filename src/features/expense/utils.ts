be21b11cc9169dea89a8bae7844d165a5d73c5cc
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 07:15:25 +0900

    feat: add getConsumptionTexts to expense/utils

--- src/features/expense/utils.ts
+++ src/features/expense/utils.ts
@@ -1,6 +1,15 @@
-import { consumptionMap } from './consts';
+import {
+  consumptionConsciousTexts,
+  consumptionEmotionalTexts,
+  consumptionEssentialTexts,
+  consumptionMap,
+  consumptionTextsMap,
+} from './consts';
 import { ConsumptionKind } from './model/types/ConsumptionKind';
 
 export function getConsumptionTitle(kind: ConsumptionKind): string {
   return consumptionMap[kind].consumptionTexts.title;
 }
+
+export const getConsumptionTexts = (kind: ConsumptionKind) =>
+  consumptionTextsMap[kind];

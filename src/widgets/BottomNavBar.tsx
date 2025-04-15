d9ac8e720661dbe9b7217a942002b88e31edbbaa
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 05:56:42 +0900

    feat: add onClick navigate to NavItem props

--- src/widgets/BottomNavBar.tsx
+++ src/widgets/BottomNavBar.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from '@tanstack/react-router';
+
 import { Button } from '@/components/ui/button';
 import Record from '@/shared/ui/icons/Record';
 import Review from '@/shared/ui/icons/Review';
@@ -6,14 +8,16 @@ import SettingsFilled from '@/shared/ui/icons/SettingsFilled';
 interface NavItemProps {
   text: string;
   icon: React.ReactNode;
+  onClick: () => void;
 }
 
-const NavItem: React.FC<NavItemProps> = ({ text, icon }) => {
+const NavItem: React.FC<NavItemProps> = ({ text, icon, onClick }) => {
   return (
     <li className='flex-1 flex flex-col py-[9px] px-5 items-center'>
       <Button
         variant='ghost'
         className='flex flex-col h-auto w-auto has-[>svg]:p-0 shadow-none items-center'
+        onClick={onClick}
       >
         {icon}
         <span className='text-[10px] text-[#222] font-medium'>{text}</span>
@@ -22,23 +26,37 @@ const NavItem: React.FC<NavItemProps> = ({ text, icon }) => {
   );
 };
 
-const navItems: NavItemProps[] = [
-  { text: '기록', icon: <Record size={24} color='#222222' /> },
-  { text: '리뷰', icon: <Review size={24} color='#222222' /> },
-  { text: '설정', icon: <SettingsFilled size={24} color='#222222' /> },
-];
-
 const BottomNavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const navItems: NavItemProps[] = [
+    {
+      text: '기록',
+      icon: <Record size={24} color='#222222' />,
+      onClick: () => void navigate({ to: '/expenses' }),
+    },
+    {
+      text: '리뷰',
+      icon: <Review size={24} color='#222222' />,
+      onClick: () => void navigate({ to: '/expenses/review' }),
+    },
+    {
+      text: '설정',
+      icon: <SettingsFilled size={24} color='#222222' />,
+      onClick: () => void navigate({ to: '/settings' }),
+    },
+  ];
+
   return (
-    <div className='fixed bottom-0 left-0 right-0 w-full min-w-[360px] max-w-[430px] mx-auto bg-white rounded-t-[16px]'>
-      <nav className='flex flex-row items-center w-full'>
-        <ul className='flex flex-row w-full'>
-          {navItems.map(({ text, icon }) => (
-            <NavItem key={text} text={text} icon={icon} />
-          ))}
-        </ul>
-      </nav>
-    </div>
+    // <div className='fixed bottom-0 left-0 right-0 w-full min-w-[360px] max-w-[430px] mx-auto bg-white rounded-t-[16px]'>
+    <nav className='flex flex-row items-center w-full mt-auto'>
+      <ul className='flex flex-row w-full'>
+        {navItems.map(({ text, icon, onClick }) => (
+          <NavItem key={text} text={text} icon={icon} onClick={onClick} />
+        ))}
+      </ul>
+    </nav>
+    // </div>
   );
 };
 export default BottomNavBar;

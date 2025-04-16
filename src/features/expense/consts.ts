eb1717e9caf67504a8e8f319f50a15d47b1639ac
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 17:26:34 +0900

    feat: carriage return in popoverText on consumptionEmotionalTexts

--- src/features/expense/consts.ts
+++ src/features/expense/consts.ts
@@ -21,7 +21,7 @@ export const consumptionEssentialTexts: ConsumptionTexts = {
 export const consumptionEmotionalTexts: ConsumptionTexts = {
   title: '감정적 소비',
   description: '필요 없거나 충동적으로 한 소비',
-  popoverText: '예시로 필요 없는 구독, 스트레스 해소용 쇼핑 등이 있어요.',
+  popoverText: '예시로 필요 없는 구독, 스트레스 해소용 쇼핑\n등이 있어요.',
 };
 
 export const consumptionConsciousTexts: ConsumptionTexts = {

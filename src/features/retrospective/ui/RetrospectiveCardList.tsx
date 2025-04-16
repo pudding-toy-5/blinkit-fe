b3a1890af8401b9a3ded87f5b69643b92b624b12
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-16 11:05:28 +0900

    feat: add RetrospectiveCardList

---
+++ src/features/retrospective/ui/RetrospectiveCardList.tsx
@@ -0,0 +1,18 @@
+import { Expense } from '@/features/expense/model/types/Expense';
+import ReviewExpenseCard from '@/features/expense/ui/ReviewExpenseCard';
+
+const RetrospectiveCardList: React.FC<{ expenses: Expense[] }> = ({
+  expenses,
+}) => {
+  return (
+    <div className='pl-5 pr-4 mt-4 pb-8 overflow-y-auto scroll'>
+      <ul className='flex flex-col gap-2'>
+        {expenses.map((expense) => (
+          <ReviewExpenseCard key={expense.uid} expense={expense} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RetrospectiveCardList;

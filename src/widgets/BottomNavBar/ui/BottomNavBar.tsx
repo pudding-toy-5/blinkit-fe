0a97693ad5eb9cb8957a94d62255e5edd45e4a18
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-17 19:59:41 +0900

    refact: move BottomNavBar, update const and type

--- src/widgets/BottomNavBar/ui/BottomNavBar.tsx
+++ src/widgets/BottomNavBar/ui/BottomNavBar.tsx
@@ -1,7 +1,6 @@
 import { useLocation, useNavigate } from '@tanstack/react-router';
 import { useEffect, useState } from 'react';
 
-import Navigation from '@/shared/model/Navigation';
 import Review from '@/shared/ui/icons/Review';
 import ReviewFilled from '@/shared/ui/icons/ReviewFilled';
 import Settings from '@/shared/ui/icons/Settings';
@@ -10,8 +9,11 @@ import Write from '@/shared/ui/icons/Write';
 import WriteFilled from '@/shared/ui/icons/WriteFilled';
 import { cn } from '@/shared/ui/styles/utils';
 
+import { type BottomNavigationType } from '../model/BottomNavigation';
+import { BottomNavigation } from '../model/BottomNavigation';
+
 interface NavItemProps {
-  nav: Navigation;
+  nav: BottomNavigationType;
   icon: React.ReactNode;
   clickedIcon: React.ReactNode;
   isClicked: boolean;
@@ -51,23 +53,25 @@ const BottomNavBar: React.FC<{ variant: 'white' | 'accent' }> = ({
   const navigate = useNavigate();
   const pathname = useLocation({ select: (location) => location.pathname });
 
-  const [selectedNav, setSelectedNav] = useState<Navigation>(Navigation.record);
+  const [selectedNav, setSelectedNav] = useState<BottomNavigationType>(
+    BottomNavigation.record
+  );
 
   const navItems: Omit<NavItemProps, 'isClicked'>[] = [
     {
-      nav: Navigation.record,
+      nav: BottomNavigation.record,
       icon: <Write size={24} color='#222222' />,
       clickedIcon: <WriteFilled size={24} color='#222222' />,
       onClick: () => void navigate({ to: '/expenses' }),
     },
     {
-      nav: Navigation.review,
+      nav: BottomNavigation.review,
       icon: <Review size={24} color='#222222' />,
       clickedIcon: <ReviewFilled size={24} color='#222222' />,
       onClick: () => void navigate({ to: '/expenses/review' }),
     },
     {
-      nav: Navigation.settings,
+      nav: BottomNavigation.settings,
       icon: <Settings size={24} color='#222222' />,
       clickedIcon: <SettingsFilled size={24} color='#222222' />,
       onClick: () => void navigate({ to: '/settings' }),
@@ -76,13 +80,13 @@ const BottomNavBar: React.FC<{ variant: 'white' | 'accent' }> = ({
 
   useEffect(() => {
     if (pathname === '/expenses') {
-      setSelectedNav(Navigation.record);
+      setSelectedNav(BottomNavigation.record);
     }
     if (pathname === '/expenses/review') {
-      setSelectedNav(Navigation.review);
+      setSelectedNav(BottomNavigation.review);
     }
     if (pathname === '/settings') {
-      setSelectedNav(Navigation.settings);
+      setSelectedNav(BottomNavigation.settings);
     }
   }, [pathname]);
 

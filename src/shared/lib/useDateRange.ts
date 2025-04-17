54a7ab0fa0c8bfe12dccfe391785ed496d17e11e
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-17 17:45:54 +0900

    feat: add useDateRange to shared/lib

---
+++ src/shared/lib/useDateRange.ts
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export default function useDateRange() {
+  const [dateRange, setDateRange] = useState<{ start: Date; end: Date }>({
+    start: new Date('2025-04-12'),
+    end: new Date('2025-04-18'),
+  });
+
+  return { dateRange, setDateRange };
+}

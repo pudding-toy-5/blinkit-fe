e9ddea74a7337642dad9e2847da6349bfe23eafb
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-17 17:51:43 +0900

    remove: unused setDateRange in useDateRange

--- src/shared/lib/useDateRange.ts
+++ src/shared/lib/useDateRange.ts
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
 export default function useDateRange() {
-  const [dateRange, setDateRange] = useState<{ start: Date; end: Date }>({
+  const [dateRange] = useState<{ start: Date; end: Date }>({
     start: new Date('2025-04-12'),
     end: new Date('2025-04-18'),
   });
 
-  return { dateRange, setDateRange };
+  return { dateRange };
 }

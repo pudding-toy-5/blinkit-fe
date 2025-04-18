63d60791316ffeefebe15a9557ac1907dd080ae8
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-18 15:54:19 +0900

    style: fix boxShadow in AddExpenseButton

--- src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
+++ src/features/expense/ui/AddExpenseButton/AddExpenseButton.tsx
@@ -6,7 +6,7 @@ const AddExpenseButton: React.FC = () => {
       aria-label='지출 추가'
       // origin- bottom-8
       className='absolute right-[20px] bottom-20 w-14 h-14 p-4 rounded-[28px] bg-[#89f336] hover:ring'
-      style={{ boxShadow: '0 8 40 0 #3A661735' }}
+      style={{ boxShadow: '0 8px 40px 0 hsba(93, 77%, 40%, 0.35) ' }}
       role='button'
     >
       <Plus size={24} />

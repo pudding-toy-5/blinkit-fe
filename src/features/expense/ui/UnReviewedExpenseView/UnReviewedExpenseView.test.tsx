01698cc6213609153b5d279688ea742440566c29
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-20 19:15:54 +0900

    test: add sanity test with tanstack-query to UnReviewedExpenseView

---
+++ src/features/expense/ui/UnReviewedExpenseView/UnReviewedExpenseView.test.tsx
@@ -0,0 +1,61 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render } from '@testing-library/react';
+import { describe, it, vi } from 'vitest';
+
+import * as expenseHooks from '@/features/expense/api/useExpenseQuery';
+import { QueryClientTestProvider } from '@/test/utils/createQueryClient';
+
+import UnReviewedExpenseView, { Props } from './UnReviewedExpenseView';
+
+vi.mock('@/features/expense/api/useExpenseQuery');
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // ✅ turns retries off
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+const mockCount = expenseHooks.useExpenseCountByRange as ReturnType<
+  typeof vi.fn
+>;
+const mockList = expenseHooks.useExpensesByRange as ReturnType<typeof vi.fn>;
+const mockUpdate = expenseHooks.useUpdateExpense as ReturnType<typeof vi.fn>;
+
+describe('UnReviewedExpenseView', () => {
+  const props: Props = {
+    onMoveRetrospective: vi.fn(),
+  };
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const renderElement = ({ onMoveRetrospective }: Props) =>
+    render(
+      <UnReviewedExpenseView onMoveRetrospective={onMoveRetrospective} />,
+      { wrapper }
+    );
+
+  it('renders with fake expense', () => {
+    const fakeExpense = {
+      uid: '1',
+      date: new Date(),
+      memo: '커피',
+      categories: [],
+      amount: 4500,
+    };
+    mockCount.mockReturnValue({ data: 1 });
+    mockList.mockReturnValue({ data: [fakeExpense] });
+
+    const { container } = renderElement({ ...props });
+
+    expect(container).toBeInTheDocument();
+  });
+});

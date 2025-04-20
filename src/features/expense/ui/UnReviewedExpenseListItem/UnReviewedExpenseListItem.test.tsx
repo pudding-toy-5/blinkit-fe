a8bf86ba60c0f8100553f8c870ce3b9975f0a8e0
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-20 18:48:46 +0900

    test: add sanity test to UnReviewedExpenseListItem

---
+++ src/features/expense/ui/UnReviewedExpenseListItem/UnReviewedExpenseListItem.test.tsx
@@ -0,0 +1,35 @@
+import { render } from '@testing-library/react';
+import { format } from 'date-fns';
+import { describe, it, vi } from 'vitest';
+
+import UnReviewedExpenseListItem, { Props } from './UnReviewedExpenseListItem';
+
+describe('UnReviewedExpenseListItem', () => {
+  const props: Props = {
+    expense: {
+      uid: 'expense-uid',
+      date: new Date(),
+      memo: '',
+      amount: 1000,
+      categories: [],
+    },
+    buttonWidth: 0,
+    onSelectReview: vi.fn(),
+  };
+
+  const renderElement = ({ expense, buttonWidth, onSelectReview }: Props) =>
+    render(
+      <UnReviewedExpenseListItem
+        expense={expense}
+        buttonWidth={buttonWidth}
+        onSelectReview={onSelectReview}
+      />
+    );
+
+  it('renders 리뷰하기 button.', () => {
+    const { getByRole } = renderElement({ ...props });
+
+    const reviewButton = getByRole('button', { name: '리뷰하기' });
+    expect(reviewButton).toBeInTheDocument();
+  });
+});

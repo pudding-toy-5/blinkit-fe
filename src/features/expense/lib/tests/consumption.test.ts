92af53e0d9bae2a45a8ae0cd5ff29d952ea6c4e1
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-21 19:10:02 +0900

    test: add test for conscious, emotional, essential consumption

--- src/features/expense/lib/tests/consumption.test.ts
+++ src/features/expense/lib/tests/consumption.test.ts
@@ -1,13 +1,25 @@
 import { consumptionMap, consumptionTextsMap } from '@/features/expense/consts';
-import {
-  getConsumptionTexts,
-  getConsumptionTitle,
-} from '@/features/expense/lib/consumption';
 import { ConsumptionKind } from '@/features/expense/model/ConsumptionKind';
 
+import { getConsumptionTexts, getConsumptionTitle } from '../consumption';
+
 describe('getConsumptionTitle', () => {
-  it('returns correct title.', () => {
+  it('returns correct title for conscious consumption.', () => {
     const kind: ConsumptionKind = ConsumptionKind.conscious;
+    const title = getConsumptionTitle(kind);
+
+    expect(title).toBe(consumptionTextsMap[kind].title);
+  });
+
+  it('returns correct title for emotional consumption.', () => {
+    const kind: ConsumptionKind = ConsumptionKind.emotional;
+    const title = getConsumptionTitle(kind);
+
+    expect(title).toBe(consumptionTextsMap[kind].title);
+  });
+
+  it('returns correct title for essential consumption.', () => {
+    const kind: ConsumptionKind = ConsumptionKind.essential;
     const title = getConsumptionTitle(kind);
 
     expect(title).toBe(consumptionTextsMap[kind].title);
@@ -23,11 +35,20 @@ describe('getConsumptionTitle', () => {
 });
 
 describe('getConsumptionTexts', () => {
-  it('returns correct texts.', () => {
+  it('returns correct texts for conscious consumption.', () => {
     const kind: ConsumptionKind = ConsumptionKind.conscious;
-    const title = getConsumptionTexts(kind);
-
-    expect(title).toStrictEqual(consumptionMap[kind].consumptionTexts);
+    const texts = getConsumptionTexts(kind);
+    expect(texts).toStrictEqual(consumptionMap[kind].consumptionTexts);
+  });
+  it('returns correct texts for essential consumption.', () => {
+    const kind: ConsumptionKind = ConsumptionKind.essential;
+    const texts = getConsumptionTexts(kind);
+    expect(texts).toStrictEqual(consumptionMap[kind].consumptionTexts);
+  });
+  it('returns correct texts for emotional consumption.', () => {
+    const kind: ConsumptionKind = ConsumptionKind.emotional;
+    const texts = getConsumptionTexts(kind);
+    expect(texts).toStrictEqual(consumptionMap[kind].consumptionTexts);
   });
 
   it('if kind is none, throws error.', () => {

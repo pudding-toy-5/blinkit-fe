17f3ff4f6a174586c616ef61cbab817d6bde9250
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-21 10:20:13 +0900

    feat: add Toaster to root route

--- src/app/routes/__root.tsx
+++ src/app/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+import Toaster from '@/shared/ui/Toaster';
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      <Toaster />
     </>
   ),
 });

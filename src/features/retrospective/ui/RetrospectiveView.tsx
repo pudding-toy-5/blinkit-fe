be0392ed972922bb41d81125db8e68e3c86df825
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-24 15:34:25 +0900

    fix: move RetrospectiveDetailPage to parent div

--- src/features/retrospective/ui/RetrospectiveView.tsx
+++ src/features/retrospective/ui/RetrospectiveView.tsx
@@ -37,71 +37,73 @@ const RetrospectiveView: React.FC<{ onMoveReview: () => void }> = ({
   }, [retrospectives]);
 
   return (
-    <div className='flex-1 flex flex-col overflow-y-auto scroll'>
-      {reviewedExpenseCount === 0 ? (
-        <div className='flex-1 flex flex-col items-center justify-center text-center'>
-          <span className='text-[15px] text-[#555] leading-[150%]'>
-            아직 리뷰한 소비가 없어요.
-            <br />
-            리뷰 탭으로 이동해 소비를 분류해주세요.
-          </span>
-          <button
-            className='text-[13px] text-[#555] rounded-full px-3 py-2 bg-[#efefef] mt-4'
-            onClick={onMoveReview}
-          >
-            리뷰 탭으로 이동하기
-          </button>
-        </div>
-      ) : (
-        <>
-          {open && consumptionKind && (
-            <RetrospectiveDetailPopoverPage
-              consumptionKind={consumptionKind}
-              onClose={() => {
-                setOpen(false);
-                setConsumptionKind(null);
+    <>
+      {open && consumptionKind && (
+        <RetrospectiveDetailPopoverPage
+          consumptionKind={consumptionKind}
+          onClose={() => {
+            setOpen(false);
+            setConsumptionKind(null);
+          }}
+        />
+      )}
+      <div className='flex-1 flex flex-col overflow-y-auto scroll'>
+        {reviewedExpenseCount === 0 ? (
+          <div className='flex-1 flex flex-col items-center justify-center text-center'>
+            <span className='text-[15px] text-[#555] leading-[150%]'>
+              아직 리뷰한 소비가 없어요.
+              <br />
+              리뷰 탭으로 이동해 소비를 분류해주세요.
+            </span>
+            <button
+              className='text-[13px] text-[#555] rounded-full px-3 py-2 bg-[#efefef] mt-4'
+              onClick={onMoveReview}
+            >
+              리뷰 탭으로 이동하기
+            </button>
+          </div>
+        ) : (
+          <>
+            <RetrospectiveCard
+              consumption={consumptionEmotional}
+              retrospective={retrospectives.find(
+                (retrospective) =>
+                  retrospective.consumptionKind === ConsumptionKind.emotional
+              )}
+              onClickRetrospectiveDetail={() => {
+                setConsumptionKind(ConsumptionKind.emotional);
+                setOpen(true);
               }}
             />
-          )}
-          <RetrospectiveCard
-            consumption={consumptionEmotional}
-            retrospective={retrospectives.find(
-              (retrospective) =>
-                retrospective.consumptionKind === ConsumptionKind.emotional
-            )}
-            onClickRetrospectiveDetail={() => {
-              setConsumptionKind(ConsumptionKind.emotional);
-              setOpen(true);
-            }}
-          />
-          <div className='h-2 bg-[#F5F3F0] shrink-0' />
-          <RetrospectiveCard
-            consumption={consumptionConscious}
-            retrospective={retrospectives.find(
-              (retrospective) =>
-                retrospective.consumptionKind === ConsumptionKind.conscious
-            )}
-            onClickRetrospectiveDetail={() => {
-              setConsumptionKind(ConsumptionKind.conscious);
-              setOpen(true);
-            }}
-          />
-          <div className='h-2 bg-[#F5F3F0] shrink-0' />
-          <RetrospectiveCard
-            consumption={consumptionEssential}
-            retrospective={retrospectives.find(
-              (retrospective) =>
-                retrospective.consumptionKind === ConsumptionKind.essential
-            )}
-            onClickRetrospectiveDetail={() => {
-              setConsumptionKind(ConsumptionKind.essential);
-              setOpen(true);
-            }}
-          />
-          <div className='h-4 w-full bg-white shrink-0' />
-        </>
-      )}
-    </div>
+            <div className='h-2 bg-[#F5F3F0] shrink-0' />
+            <RetrospectiveCard
+              consumption={consumptionConscious}
+              retrospective={retrospectives.find(
+                (retrospective) =>
+                  retrospective.consumptionKind === ConsumptionKind.conscious
+              )}
+              onClickRetrospectiveDetail={() => {
+                setConsumptionKind(ConsumptionKind.conscious);
+                setOpen(true);
+              }}
+            />
+            <div className='h-2 bg-[#F5F3F0] shrink-0' />
+            <RetrospectiveCard
+              consumption={consumptionEssential}
+              retrospective={retrospectives.find(
+                (retrospective) =>
+                  retrospective.consumptionKind === ConsumptionKind.essential
+              )}
+              onClickRetrospectiveDetail={() => {
+                setConsumptionKind(ConsumptionKind.essential);
+                setOpen(true);
+              }}
+            />
+            <div className='h-4 w-full bg-white shrink-0' />
+          </>
+        )}
+      </div>
+    </>
   );
 };
 

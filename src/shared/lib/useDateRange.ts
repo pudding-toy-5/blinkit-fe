d5b64590a932e0dc3d281b03cbce5b9a401a7413
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-28 08:41:02 +0900

    refact: useDateRange to use DateRange from react-day-picker

--- src/shared/lib/useDateRange.ts
+++ src/shared/lib/useDateRange.ts
@@ -1,10 +1,11 @@
 import { useState } from 'react';
+import { DateRange } from 'react-day-picker';
 
 export default function useDateRange() {
-  const [dateRange] = useState<{ start: Date; end: Date }>({
-    start: new Date('2025-04-01'),
-    end: new Date('2025-05-31'),
+  const [dateRange, setDateRange] = useState<DateRange | undefined>({
+    from: new Date('2025-04-01'),
+    to: new Date('2025-05-31'),
   });
 
-  return { dateRange };
+  return { dateRange, setDateRange };
 }

4c18c9e453c46c00405c8efa1ba6567ab3e3b6c6
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-27 17:58:42 +0900

    refact: add condition on isRecordEmpty on RetrospectiveView

--- src/features/retrospective/ui/RetrospectiveView.tsx
+++ src/features/retrospective/ui/RetrospectiveView.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@tanstack/react-router';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import {
   consumptionConscious,
@@ -12,6 +12,7 @@ import useDateRange from '@/shared/lib/useDateRange';
 
 import RetrospectiveCard from './RetrospectiveCard';
 import RetrospectiveDetailPopoverPage from './RetrospectiveDetailPopoverPage';
+import RetrospectiveHeader from './RetrospectiveHeader';
 
 const RetrospectiveView: React.FC = () => {
   const {
@@ -23,17 +24,34 @@ const RetrospectiveView: React.FC = () => {
     end,
   });
 
-  const [consumptionKind, setConsumptionKind] =
-    useState<ConsumptionKind | null>(null);
+  const [consumptionKind, setConsumptionKind] = useState<ConsumptionKind>(
+    ConsumptionKind.none
+  );
   const [open, setOpen] = useState<boolean>(false);
 
-  const [reviewedExpenseCount, setReviewedExpenseCount] = useState<number>(0);
+  const isRecordEmpty = useMemo(
+    () => retrospectives.reduce((acc, cur) => (acc += cur.totalCount), 0) === 0,
+    [retrospectives]
+  );
 
-  useEffect(() => {
-    setReviewedExpenseCount(
-      retrospectives.reduce((acc, cur) => acc + cur.totalCount, 0)
+  if (isRecordEmpty) {
+    return (
+      <div className='flex-1 flex flex-col overflow-y-auto scroll'>
+        <div className='flex-1 flex flex-col items-center justify-center text-center'>
+          <span className='text-[15px] text-[#555] leading-[150%]'>
+            아직 리뷰한 소비가 없어요.
+            <br />
+            기록 화면으로 이동해 소비를 분류해주세요.
+          </span>
+          <Link to='/expenses'>
+            <button className='text-[13px] text-[#555] rounded-full px-3 py-2 bg-[#efefef] mt-4'>
+              기록 화면으로 이동하기
+            </button>
+          </Link>
+        </div>
+      </div>
     );
-  }, [retrospectives]);
+  }
 
   return (
     <>
@@ -42,64 +60,71 @@ const RetrospectiveView: React.FC = () => {
           consumptionKind={consumptionKind}
           onClose={() => {
             setOpen(false);
-            setConsumptionKind(null);
+            setConsumptionKind(ConsumptionKind.none);
           }}
         />
       )}
       <div className='flex-1 flex flex-col overflow-y-auto scroll'>
-        {reviewedExpenseCount === 0 ? (
-          <div className='flex-1 flex flex-col items-center justify-center text-center'>
-            <span className='text-[15px] text-[#555] leading-[150%]'>
-              아직 리뷰한 소비가 없어요.
-              <br />
-              기록 화면으로 이동해 소비를 분류해주세요.
-            </span>
-            <Link to='/expenses'>
-              <button className='text-[13px] text-[#555] rounded-full px-3 py-2 bg-[#efefef] mt-4'>
-                기록 화면으로 이동하기
-              </button>
-            </Link>
-          </div>
-        ) : (
-          <>
-            <RetrospectiveCard
-              consumption={consumptionEmotional}
-              retrospective={retrospectives.find(
-                (retrospective) =>
-                  retrospective.consumptionKind === ConsumptionKind.emotional
-              )}
-              onClickRetrospectiveDetail={() => {
-                setConsumptionKind(ConsumptionKind.emotional);
-                setOpen(true);
-              }}
-            />
-            <div className='h-2 bg-[#F5F3F0] shrink-0' />
-            <RetrospectiveCard
-              consumption={consumptionConscious}
-              retrospective={retrospectives.find(
-                (retrospective) =>
-                  retrospective.consumptionKind === ConsumptionKind.conscious
-              )}
-              onClickRetrospectiveDetail={() => {
-                setConsumptionKind(ConsumptionKind.conscious);
-                setOpen(true);
-              }}
-            />
-            <div className='h-2 bg-[#F5F3F0] shrink-0' />
-            <RetrospectiveCard
-              consumption={consumptionEssential}
-              retrospective={retrospectives.find(
+        {/* <SelectPeriodDrawer /> */}
+        <RetrospectiveHeader
+          amount={{
+            total: retrospectives.reduce(
+              (acc, cur) => (acc += cur.totalAmount),
+              0
+            ),
+            essential:
+              retrospectives.find(
                 (retrospective) =>
                   retrospective.consumptionKind === ConsumptionKind.essential
-              )}
-              onClickRetrospectiveDetail={() => {
-                setConsumptionKind(ConsumptionKind.essential);
-                setOpen(true);
-              }}
-            />
-            <div className='h-4 w-full bg-white shrink-0' />
-          </>
-        )}
+              )?.totalAmount ?? 0,
+            conscious:
+              retrospectives.find(
+                (retrospective) =>
+                  retrospective.consumptionKind === ConsumptionKind.conscious
+              )?.totalAmount ?? 0,
+            emotional:
+              retrospectives.find(
+                (retrospective) =>
+                  retrospective.consumptionKind === ConsumptionKind.emotional
+              )?.totalAmount ?? 0,
+          }}
+        />
+        <RetrospectiveCard
+          consumption={consumptionEmotional}
+          retrospective={retrospectives.find(
+            (retrospective) =>
+              retrospective.consumptionKind === ConsumptionKind.emotional
+          )}
+          onClickRetrospectiveDetail={() => {
+            setConsumptionKind(ConsumptionKind.emotional);
+            setOpen(true);
+          }}
+        />
+        <div className='h-2 bg-[#F5F3F0] shrink-0' />
+        <RetrospectiveCard
+          consumption={consumptionConscious}
+          retrospective={retrospectives.find(
+            (retrospective) =>
+              retrospective.consumptionKind === ConsumptionKind.conscious
+          )}
+          onClickRetrospectiveDetail={() => {
+            setConsumptionKind(ConsumptionKind.conscious);
+            setOpen(true);
+          }}
+        />
+        <div className='h-2 bg-[#F5F3F0] shrink-0' />
+        <RetrospectiveCard
+          consumption={consumptionEssential}
+          retrospective={retrospectives.find(
+            (retrospective) =>
+              retrospective.consumptionKind === ConsumptionKind.essential
+          )}
+          onClickRetrospectiveDetail={() => {
+            setConsumptionKind(ConsumptionKind.essential);
+            setOpen(true);
+          }}
+        />
+        <div className='h-4 w-full bg-white shrink-0' />
       </div>
     </>
   );

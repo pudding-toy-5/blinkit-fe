406754d256dd6cbcbbb9811b129ed43b59af1035
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-28 13:08:08 +0900

    feat: change useDateRange default value to undefined

--- src/shared/lib/useDateRange.ts
+++ src/shared/lib/useDateRange.ts
@@ -2,10 +2,7 @@ import { useState } from 'react';
 import { DateRange } from 'react-day-picker';
 
 export default function useDateRange() {
-  const [dateRange, setDateRange] = useState<DateRange | undefined>({
-    from: new Date('2025-04-01'),
-    to: new Date('2025-05-31'),
-  });
+  const [dateRange, setDateRange] = useState<DateRange | undefined>(undefined);
 
   return { dateRange, setDateRange };
 }

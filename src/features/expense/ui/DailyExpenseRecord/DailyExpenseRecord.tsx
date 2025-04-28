118150b40da846c88ef3f927a63b19ebc6f039db
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-28 16:44:35 +0900

    Revert "Merge pull request #240 from pudding-toy-5/feat/add-expense-card-isReview"
    This reverts commit 0f823543ea578f52a240effb131935311959af61, reversing
    changes made to 5511758dd48c497acea9f3180c2040db1a914110.

--- src/features/expense/ui/DailyExpenseRecord/DailyExpenseRecord.tsx
+++ src/features/expense/ui/DailyExpenseRecord/DailyExpenseRecord.tsx
@@ -1,53 +1,34 @@
 import { useNavigate } from '@tanstack/react-router';
 
 import CategoryTag from '@/features/category/ui/CategoryTag';
-import { ConsumptionKind } from '@/features/expense/model/ConsumptionKind';
 import { Expense } from '@/features/expense/model/Expense';
-import { cn } from '@/shared/ui/styles/utils';
 
-const ExpenseCard: React.FC<Omit<Expense, 'date'>> = ({
+const DailyExpenseRecord: React.FC<Omit<Expense, 'date'>> = ({
   uid,
   categories,
   amount,
   memo,
-  consumptionKind,
 }) => {
   const navigate = useNavigate();
 
   const onClick = () => {
     void navigate({ to: '/expenses/$uid', params: { uid } });
   };
-
   return (
     <li
       aria-labelledby={`expense-${uid}`}
       className='flex flex-col p-4 rounded-[8px] bg-white'
       onClick={onClick}
     >
-      <div className='flex flex-row gap-1 items-center'>
-        <span aria-label='지출 금액' className='text-[15px] font-semibold'>
-          {amount.toLocaleString()}원
-        </span>
-        <span
-          aria-label='리뷰 여부'
-          className={cn(
-            'text-[13px] text-[#555]',
-            'px-[6px] py-[2px]',
-            'rounded-[4px]',
-            'bg-[#f5f3f0]'
-          )}
-        >
-          {!consumptionKind || consumptionKind === ConsumptionKind.none
-            ? '리뷰안함'
-            : '리뷰완료'}
-        </span>
-      </div>
-      <span
+      <p aria-label='지출 금액' className='text-[15px] font-semibold'>
+        {amount.toLocaleString()}원
+      </p>
+      <p
         aria-label='메모'
         className='text-[15px] leading-[150%] mt-2 break-words'
       >
         {memo}
-      </span>
+      </p>
       <div className='flex flex-row flex-wrap gap-x-1 gap-y-1.5 mt-6 w-full'>
         {categories.map((category) => (
           <CategoryTag key={category.uid} tagName={category.name} />
@@ -57,4 +38,4 @@ const ExpenseCard: React.FC<Omit<Expense, 'date'>> = ({
   );
 };
 
-export default ExpenseCard;
+export default DailyExpenseRecord;

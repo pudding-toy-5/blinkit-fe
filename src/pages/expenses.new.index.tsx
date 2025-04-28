3d4f7aeca8ec72bd85891c5fa0a43cb0231e825f
Dokyun Claire Lim <im.koremp@gmail.com>
2025-04-28 18:50:04 +0900

    fix: add temporary updateExpense with consumption kind after addExpense

--- src/pages/expenses.new.index.tsx
+++ src/pages/expenses.new.index.tsx
@@ -2,7 +2,11 @@ import { createFileRoute, useNavigate } from '@tanstack/react-router';
 import { toast } from 'sonner';
 
 import AuthGuard from '@/features/auth/ui/AuthGuard';
-import { useAddExpense } from '@/features/expense/api/useExpenseQuery';
+import {
+  useAddExpense,
+  useUpdateExpense,
+} from '@/features/expense/api/useExpenseQuery';
+import { ConsumptionKind } from '@/features/expense/model/ConsumptionKind';
 import { Expense } from '@/features/expense/model/Expense';
 import ExpenseForm from '@/features/expense/ui/ExpenseForm';
 import SubPageHeader from '@/shared/ui/SubPageHeader';
@@ -18,21 +22,36 @@ export const Route = createFileRoute('/expenses/new/')({
 export function RouteComponent() {
   const navigate = useNavigate();
   const addExpense = useAddExpense();
+  const updateExpense = useUpdateExpense();
 
   const initialExpense: Omit<Expense, 'uid'> = {
     date: new Date(),
     memo: '',
     categories: [],
     amount: 0,
+    consumptionKind: ConsumptionKind.none,
   };
 
   const handleSubmit = (expense: Omit<Expense, 'uid'>) => {
+    const consumptionKind = expense.consumptionKind ?? ConsumptionKind.none;
+
     addExpense.mutate(
       { ...expense },
       {
-        onSuccess: () => {
-          toast.success('지출내역을 추가했어요.');
-          void navigate({ to: '/expenses' });
+        onSuccess: (data) => {
+          // todo: remove this
+          updateExpense.mutate(
+            { ...expense, uid: data.uid, consumptionKind },
+            {
+              onSuccess: () => {
+                toast.success('지출내역을 추가했어요.');
+                void navigate({ to: '/expenses' });
+              },
+              onError: () => {
+                toast.error('지출내역을 추가하는데 실패했어요.');
+              },
+            }
+          );
         },
         onError: () => {
           toast.error('지출내역을 추가하는데 실패했어요.');

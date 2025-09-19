db6d5879aa6c80a061168a282f4a8e992b8035a1
Dokyun Claire Lim <im.koremp@gmail.com>
2025-09-19 18:35:20 +0900

    test: add YearMonthTrigger test

---
+++ src/widgets/YearMonthPicker/__tests__/YearMonthTrigger.test.tsx
@@ -0,0 +1,45 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+
+import YearMonth from '@/shared/model/YearMonth';
+
+import type { YearMonthTriggerProps } from '../types';
+import YearMonthTrigger from '../YearMonthTrigger';
+
+describe('YearMonthTrigger', () => {
+  const current = new Date();
+  const currentYearMonth: YearMonth = {
+    year: current.getFullYear(),
+    month: current.getMonth() + 1,
+  };
+
+  const renderElement = ({ yearMonth }: YearMonthTriggerProps) =>
+    render(<YearMonthTrigger yearMonth={yearMonth} />);
+
+  it('when yearMonth year is current year, renders only month.', () => {
+    const { getByRole } = renderElement({
+      yearMonth: currentYearMonth,
+    });
+
+    const trigger = getByRole('button');
+    expect(trigger).toBeInTheDocument();
+    expect(trigger).not.toHaveTextContent(currentYearMonth.year.toString());
+    expect(trigger).toHaveTextContent(currentYearMonth.month.toString() + '월');
+  });
+
+  it('when yearMonth year is not current year, renders year + month.', () => {
+    const pastYearMonth: YearMonth = {
+      ...currentYearMonth,
+      year: currentYearMonth.year - 1,
+    };
+
+    const { getByRole } = renderElement({
+      yearMonth: pastYearMonth,
+    });
+
+    const trigger = getByRole('button');
+    expect(trigger).toBeInTheDocument();
+    expect(trigger).toHaveTextContent(`${pastYearMonth.year.toString()}년 `);
+    expect(trigger).toHaveTextContent(`${pastYearMonth.month.toString()}월`);
+  });
+});

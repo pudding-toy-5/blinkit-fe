8355af188e0c2a268dea76a661522d6214daceea
Dokyun Claire Lim <im.koremp@gmail.com>
2025-09-30 17:16:38 +0900

    chore: fix vitest.setup errors

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/vitest';
 
 import { cleanup } from '@testing-library/react';
-import { afterEach } from 'vitest';
+import { afterEach, beforeEach, vi } from 'vitest';
 
 beforeEach(() => {
   Object.defineProperty(window, 'matchMedia', {

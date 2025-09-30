6fefefa6692ae8bd3fcc5ed107bae736bfefd119
Dokyun Claire Lim <im.koremp@gmail.com>
2025-09-30 22:14:54 +0900

    feat: add YearMonthList

---
+++ src/widgets/YearMonthPicker/YearMonthList.tsx
@@ -0,0 +1,42 @@
+import { getMonthList } from '@/shared/lib/dateUtils';
+
+import YearMonthListItem from './YearMonthListItem';
+
+export interface YearMonthListProps {
+  minDate: Date;
+  maxDate: Date;
+  selected: Date;
+  onSelect: (date: Date) => void;
+}
+
+const YearMonthList: React.FC<YearMonthListProps> = ({
+  minDate,
+  maxDate,
+  selected,
+  onSelect,
+}) => {
+  const months: Date[] = getMonthList(minDate, maxDate).reverse();
+
+  return (
+    <ul className='list-none flex flex-col gap-6 max-h-60 overflow-y-auto scroll'>
+      {months.map((date) => {
+        const isSameYearMonth =
+          date.getFullYear() === selected.getFullYear() &&
+          date.getMonth() === selected.getMonth();
+
+        return (
+          <YearMonthListItem
+            key={`${date.getFullYear().toString()}-${date.getMonth().toString()}`}
+            date={date}
+            isSelected={isSameYearMonth}
+            onClick={() => {
+              onSelect(date);
+            }}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default YearMonthList;

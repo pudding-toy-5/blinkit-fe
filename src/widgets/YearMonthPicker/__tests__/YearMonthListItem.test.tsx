7f86b223d5ea84dc81765d0f45d9baa5b43af122
Dokyun Claire Lim <im.koremp@gmail.com>
2025-09-30 22:15:34 +0900

    test: add base YearMonthListItem

---
+++ src/widgets/YearMonthPicker/__tests__/YearMonthListItem.test.tsx
@@ -0,0 +1,8 @@
+describe('YearMonthListItem', () => {
+  it('renders year and month.', () => {});
+
+  describe('selected', () => {
+    it('when listitem is selected, shows selected icon.', () => {});
+    it('when listitem is not selected, does not show selected icon.', () => {});
+  });
+});

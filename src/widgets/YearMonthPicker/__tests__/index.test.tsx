74863496c5ceb039e64589a573d32939866b26a9
Dokyun Claire Lim <im.koremp@gmail.com>
2025-09-30 22:15:14 +0900

    test: add base YearMonthPicker

---
+++ src/widgets/YearMonthPicker/__tests__/index.test.tsx
@@ -0,0 +1,20 @@
+describe('YearMonthPicker', () => {
+  describe('Previous Button', () => {
+    it('is disabled when value date is same minDate year and month.', () => {});
+    it('is activated when value date is not same minDate year and month.', () => {});
+
+    it('when clicked, changes value to previous month.');
+  });
+
+  describe('Next Button', () => {
+    it('is disabled when value date is same minDate year and month.', () => {});
+    it('is activated when value date is not same minDate year and month.', () => {});
+
+    it('when clicked, changes value to next month.');
+  });
+
+  it('when value year is current year, trigger renders only month.');
+  it('when value year is not current year, trigger renders year and month.');
+
+  it('when trigger clicked, renders drawer content.');
+});

dc18126a516f0c186005db78e9e44d8d193326eb
Dokyun Claire Lim <im.koremp@gmail.com>
2025-10-10 18:39:57 +0900

    test: add YearMonthListItem

--- src/widgets/YearMonthPicker/__tests__/YearMonthListItem.test.tsx
+++ src/widgets/YearMonthPicker/__tests__/YearMonthListItem.test.tsx
@@ -1,8 +1,48 @@
+import { fireEvent, queryByLabelText, render } from '@testing-library/react';
+
+import YearMonthListItem, {
+  YearMonthListItemProps,
+} from '../YearMonthListItem';
+
 describe('YearMonthListItem', () => {
-  it('renders year and month.', () => {});
+  const props: YearMonthListItemProps = {
+    date: new Date(),
+    isSelected: false,
+    onClick: vi.fn(),
+  };
+
+  const renderElement = ({
+    date,
+    isSelected,
+    onClick,
+  }: YearMonthListItemProps) =>
+    render(
+      <YearMonthListItem
+        date={date}
+        isSelected={isSelected}
+        onClick={onClick}
+      />
+    );
+
+  it('renders year and month.', () => {
+    const { getByText } = renderElement({ ...props });
+
+    const dateString = `${props.date.getFullYear().toString()}년 ${(props.date.getMonth() + 1).toString()}월`;
+    const dateText = getByText(dateString);
+    expect(dateText).toBeInTheDocument();
+  });
 
   describe('selected', () => {
-    it('when listitem is selected, shows selected icon.', () => {});
-    it('when listitem is not selected, does not show selected icon.', () => {});
+    it('when listitem is selected, shows selected icon.', () => {
+      const { getByLabelText } = renderElement({ ...props, isSelected: true });
+      const selectedIcon = getByLabelText('selected icon');
+      expect(selectedIcon).toBeInTheDocument();
+    });
+
+    it('when listitem is not selected, does not show selected icon.', () => {
+      const { queryByLabelText } = renderElement({ ...props });
+      const selectedIcon = queryByLabelText('selected icon');
+      expect(selectedIcon).not.toBeInTheDocument();
+    });
   });
 });
